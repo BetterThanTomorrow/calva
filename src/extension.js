f0f78630c9fb91563c74f242d192ea87182a0359
Stian Sivertsen <stian-sivertsen@hotmail.com>
2017-01-06 00:37:23 +0100

    Possible to load/evaluate current file with a command. closes #11  - Now also provides a msg if it fails to evaluate.

--- src/extension.js
+++ src/extension.js
@@ -16,10 +16,10 @@ function updateStatusbar(state) {
     statusbar_type.text = state.session_type.statusbar;
     switch (state.session_type.id) {
         case SESSION_TYPE.CLJ.id:
-            statusbar_type.color = "rgb(88,129,216)";
+            statusbar_type.color = "rgb(144,180,254)";
             break;
         case SESSION_TYPE.CLJS.id:
-            statusbar_type.color = "rgb(99,177,50)";
+            statusbar_type.color = "rgb(145,220,71)";
             break;
         default:
             statusbar_type.color = "rgb(192,192,192)";
@@ -42,9 +42,8 @@ function findSession(state, current, sessions) {
                     } else if (result.ex) {
                         console.log("EXCEPTION!! HANDLE IT");
                         console.log(JSON.stringify(result));
-                    } else if (result.status && result.status[0] === "done") {
-                        tmpClient.end();
                     }
+                    tmpClient.end();
                 }
             });
         })
@@ -116,20 +115,62 @@ function activate(context) {
                                     if(result.hasOwnProperty('value')) {
                                         vscode.window.showInformationMessage("=> " + (result.value.length > 0 ? result.value : "no result.."));
                                     } else if (result.ex) {
+                                        vscode.window.showErrorMessage("Error evaluating the selected expressions");
                                         console.log("EXCEPTION!! HANDLE IT");
                                         console.log(JSON.stringify(result));
-                                    } else if (result.status && result.status[0] === "done") {
-                                        evalClient.end();
                                     }
+                                    evalClient.end();
                                 }
                             });
                         });
                     }
+                } else {
+                    vscode.window.showErrorMessage("Filetype " + filetype + " not supported by current nREPL => " + state.session_type.statusbar);
                 }
             }
         }
     });
     context.subscriptions.push(evaluateExpression);    
+
+    let evaluateFile = vscode.commands.registerCommand('visualclojure.evaluateFile', function () {
+        if(state.connected) {
+            let editor = vscode.window.activeTextEditor;
+            if (editor !== undefined) {
+                let filetypeIndex = (editor.document.fileName.lastIndexOf('.') + 1);
+                let filetype = editor.document.fileName.substr(filetypeIndex,
+                                                               editor.document.fileName.length);
+                if(state.session_type.supports.indexOf(filetype) >= 0) {
+                    let documentText = editor.document.getText();
+                    let hasText = documentText.length > 0;
+                    if (hasText) {
+                        let fileNameIndex = (editor.document.fileName.lastIndexOf('\\') + 1);
+                        let fileName = editor.document.fileName.substr(fileNameIndex,       
+                                                                       editor.document.fileName.length);
+                        let filePath = editor.document.fileName;
+                        let evalClient = nreplClient.connect({host: state.hostname, port: state.port}).once('connect', function() {
+                            evalClient.loadFile(documentText, fileName, filePath, state.session, function (err, results) {
+                                for(var r = 0; r < results.length; r++) {
+                                    let result = results[r];
+                                    console.log(JSON.stringify(result));
+                                    if(result.hasOwnProperty('value')) {
+                                        vscode.window.showInformationMessage("=> " + (result.value.length > 0 ? result.value : "no result.."));
+                                    } else if (result.ex) {
+                                        vscode.window.showErrorMessage("Error evaluating " + fileName);
+                                        console.log("EXCEPTION!! HANDLE IT");
+                                        console.log(JSON.stringify(result));
+                                    }
+                                    evalClient.end();
+                                }
+                            });
+                        });
+                    }
+                } else {
+                    vscode.window.showErrorMessage("Filetype " + filetype + " not supported by current repl => " + state.session_type.statusbar);
+                }
+            }
+        }
+    });
+    context.subscriptions.push(evaluateFile);    
 }
 exports.activate = activate;
 
