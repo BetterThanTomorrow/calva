cce6b464add060aa533d45d0ac716dc716070534
Stian Sivertsen <stian-sivertsen@hotmail.com>
2017-01-11 21:08:41 +0100

    Implemented basic exception-handling for both clj and cljs. Closes #14 and #20

--- src/extension.js
+++ src/extension.js
@@ -175,39 +175,95 @@ function getContentToPreviousBracket(block) {
     return block.substr(currPos + 1, block.length);
 };
 
-function handleException(ex) {
+function handleException(exceptions, isSelection = false) {
+    let errorHasBeenMarked = false;
     diagnosticCollection.clear();
     let exClient = nreplClient.create({
         host: state.hostname,
         port: state.port
     }).once('connect', function () {
-        let msg = nreplMsg.stacktrace(state);
+        let msg = nreplMsg.stacktrace(state),
+            editor = vscode.window.activeTextEditor,
+            errLine = null,
+            errChar = null,
+            errFileUri = editor.document.uri;
+
         exClient.send(msg, (results) => {
-            for(let r = 0; r < results.length; r++) {
-                let result = results[r];
-                console.log("new ex:");
-                console.log(result);
-                
-                let errLine = result.line - 1,
-                    errChar = result.column - 1,
-                    errFile = result.file,
-                    errFileUri = null,
-                    errMsg = result.message,
-                    editor = vscode.window.activeTextEditor;
+            if (results.length === 2 && results[0].status[0] === "no-error" && results[1].status[0] === "done") {
+                let errorMsg = "Error when evaluating this expression..";
+                for(let r = 0; r < exceptions.length; r++) {
+                    let result = exceptions[r];
+                    if(result.hasOwnProperty('err')
+                      && result.err.indexOf("line") !== -1 
+                      && result.err.indexOf("column") !== -1) {
+                        errorHasBeenMarked = true;  
+                        let errorParts = result.err;
+                        if (errorParts.indexOf("starting at line") !== -1 && errorParts.indexOf("and column") !== -1) {
+                            errorParts = result.err.split(' ');
+                            errorMsg = result.err.substring(result.err.indexOf("clojure.lang.ExceptionInfo:") + 27, result.err.indexOf("starting"));
+                        } else if (errorParts.indexOf("at line") !== -1 && errorParts.indexOf("and column") === -1) {
+                            errorParts = result.err.substring(result.err.indexOf('{'), result.err.indexOf('}')).replace(/:/g, '').replace(/,/g, '').replace(/\r\n/, '').replace(/}/, '').split(' ');
+                            errorMsg = result.err.substring(result.err.indexOf("clojure.lang.ExceptionInfo:") + 27, result.err.indexOf("at line"));
+                        } else if (errorParts.indexOf(":line") !== -1 && errorParts.indexOf(":column") !== -1) {
+                            errorParts = result.err.substring(result.err.indexOf('{'), result.err.indexOf('}')).replace(/:/g, '').replace(/,/g, '').replace(/\r\n/, '').replace(/}/, '').split(' ');
+                            errorMsg = result.err.substring(result.err.indexOf("clojure.lang.ExceptionInfo:") + 27, result.err.indexOf("{"));
+                        }
+                        errLine = parseInt(errorParts[errorParts.indexOf("line") + 1], 10) - 1;
+                        errChar = parseInt(errorParts[errorParts.indexOf("column") + 1], 10) - 1;
+                    }
+                    if (result.hasOwnProperty('err') && result.err.indexOf("WARNING:") !== -1) {
+                        errorMsg += "\n" + result.err.substring(result.err.indexOf("WARNING:"), result.err.indexOf("at line"));
+                    }
 
-                if (errFile) {
-                    errFileUri = vscode.Uri.file(errFile);
-                } else {
-                    errFileUri = editor.document.uri;
                 }
-
-                if(errLine >= 0  && errChar >= 0) {
-                    let errPos = new vscode.Position(errLine, errChar);
+                if(!errorHasBeenMarked) {
+                    diagnosticCollection.set(editor.document.uri, 
+                                                [new vscode.Diagnostic(new vscode.Range(editor.selection.start.line, 
+                                                editor.selection.start.character, 
+                                                editor.selection.start.line, 
+                                                editor.document.lineAt(editor.selection.start.line).text.length),
+                                errorMsg, vscode.DiagnosticSeverity.Error)]);
+                } else if(errLine >= 0  && errChar >= 0) {
+                    if(isSelection) {
+                        errLine = errLine + editor.selection.start.line;
+                        errChar = errChar + editor.selection.start.character;
+                    }
+                    let errPos = new vscode.Position(errLine, errChar),
+                        errLineLength = editor.document.lineAt(errLine).text.length;
+                    
                     editor.selection = new vscode.Selection(errPos, errPos);
-                    let errLineLength = editor.document.lineAt(errLine).text.length;
-
                     diagnosticCollection.set(errFileUri, [new vscode.Diagnostic(new vscode.Range(errLine, errChar, errLine, errLineLength),
-                        errMsg, vscode.DiagnosticSeverity.Error)]);
+                        errorMsg, vscode.DiagnosticSeverity.Error)]);
+                }
+            } else {
+                for(let r = 0; r < results.length; r++) {
+                    let result = results[r],
+                        errLine = result.line - 1,
+                        errChar = result.column - 1,
+                        errFile = result.file,
+                        errFileUri = null,
+                        errMsg = result.message,
+                        editor = vscode.window.activeTextEditor;
+
+                    if (errFile) {
+                        errFileUri = vscode.Uri.file(errFile);
+                    } else {
+                        errFileUri = editor.document.uri;
+                    }
+
+                    if(errLine >= 0  && errChar >= 0) {
+                        if(!editor.selection.isEmpty) {
+                            errLine = errLine + editor.selection.start.line;
+                            errChar = errChar + editor.selection.start.character;
+                        }
+
+                        let errPos = new vscode.Position(errLine, errChar);
+                        editor.selection = new vscode.Selection(errPos, errPos);
+                        let errLineLength = editor.document.lineAt(errLine).text.length;
+
+                        diagnosticCollection.set(errFileUri, [new vscode.Diagnostic(new vscode.Range(errLine, errChar, errLine, errLineLength),
+                            errMsg, vscode.DiagnosticSeverity.Error)]);
+                    }
                 }
             }
             exClient.end();
@@ -314,9 +370,11 @@ function activate(context) {
                                         outputChannel.appendLine("=>")
                                         outputChannel.appendLine((result.value.length > 0 ? result.value : "no result.."));
                                     } else if (result.ex) {
-                                        vscode.window.showErrorMessage("Error evaluating the selected expressions");
-                                        console.log("EXCEPTION!! HANDLE IT");
-                                        console.log(JSON.stringify(result));
+                                        vscode.window.showErrorMessage("Error evaluating the selected expression(s)");
+                                        outputChannel.appendLine("Evaluation: failure");
+                                        outputChannel.appendLine("=>");
+                                        outputChannel.appendLine(result.ex);
+                                        handleException(results, true);
                                     }
                                 }
                                 outputChannel.appendLine("----------- done -----------\n");
@@ -352,6 +410,7 @@ function activate(context) {
                         outputChannel.appendLine("Evaluating  " + fileName);
                         outputChannel.appendLine("----------------------------");
 
+                        diagnosticCollection.clear();
                         let evalClient = nreplClient.create({
                             host: state.hostname,
                             port: state.port
@@ -372,13 +431,7 @@ function activate(context) {
                                         outputChannel.appendLine("Evaluation: failure");
                                         outputChannel.appendLine("=>");
                                         outputChannel.appendLine(result.ex);
-                                        console.log(JSON.stringify(results));
-                                        console.log(JSON.stringify(result));
-                                        
-                                        console.log(JSON.stringify(state));
-                                        console.log(result.ex.session);
-
-                                        handleException(result);
+                                        handleException(results);
                                     }
                                 }
                                 outputChannel.appendLine("----------- done -----------\n");
