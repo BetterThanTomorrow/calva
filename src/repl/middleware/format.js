fea3ed42c0ee0fdf86b074f23ee0dc309fdf2391
Stian Sivertsen <sivertsenstian@gmail.com>
2017-08-20 20:31:53 +0200

    Fixed an issue where a reformat would trigger infinitly.

--- src/repl/middleware/format.js
+++ src/repl/middleware/format.js
@@ -3,9 +3,14 @@ const state = require ('../../state');
 const repl = require('../client');
 const message = require('../message');
 const {getDocument, getFileType} = require('../../utilities');
+const ignoreNextSave = [];
 
-function formatDocument(document = {}) {
+function formatDocument(document) {
     let current = state.deref();
+    if (document.languageId !== 'clojure' || ignoreNextSave.length > 0) {
+        return;
+    }
+
     if(current.get('connected')) {
         let doc = getDocument(document);
         let client = repl.create().once('connect', () => {
@@ -14,8 +19,16 @@ function formatDocument(document = {}) {
                        session: current.get(getFileType(doc))};
             client.send(msg, function (results) {
                 const wholeDocument = new vscode.Range(0, 0, doc.lineCount, doc.getText().length);
-                let formattedDocument = results[0]["formatted-code"];
-                vscode.window.activeTextEditor.edit(editor => editor.replace(wholeDocument, formattedDocument));
+                let formattedDocument = results[0]["formatted-code"],
+                    active = vscode.window.activeTextEditor;
+
+                if (active.document === document) {
+                    active.edit(editor => editor.replace(wholeDocument, formattedDocument));
+                    ignoreNextSave.push(document);
+                    document.save().then(() => {
+                        ignoreNextSave.pop();
+                    });;
+                }
                 client.end();
             });
         });
