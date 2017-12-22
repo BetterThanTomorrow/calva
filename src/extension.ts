4be4c23f194e32878f22a41ef8e3d64823572bc4
Nikita Prokopov <prokopov@gmail.com>
2017-12-22 14:29:57 +0300

    jump to matching bracket commands

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,12 @@
 import * as vscode from 'vscode';
-import { Position, Range } from 'vscode';
+import { Position, Range, Selection } from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
 	const pairs = { ")": "(", "]": "[", "}": "{"};	
 	function opening(char) { return char==="(" || char==="[" || char==="{"; }
 	function closing(char) { return char===")" || char==="]" || char==="}"; }
 	function position_str(pos: Position) { return "" + pos.line + ":" + pos.character; }
+	function is_clojure(editor) { return !!editor && editor.document.languageId === "clojure"; } 
 
 	let activeEditor:  vscode.TextEditor = vscode.window.activeTextEditor,
 			configuration: vscode.WorkspaceConfiguration,
@@ -20,17 +21,18 @@ export function activate(context: vscode.ExtensionContext) {
 
 	vscode.window.onDidChangeActiveTextEditor(editor => {
 		activeEditor = editor;
-		if (editor)
+		// console.log("onDidChangeActiveTextEditor", editor.document.languageId);
+		if (is_clojure(editor))
 			scheduleRainbowBrackets();
 	}, null, context.subscriptions);
 
 	vscode.window.onDidChangeTextEditorSelection(event => {
-		if (event.textEditor === vscode.window.activeTextEditor)
+		if (event.textEditor === vscode.window.activeTextEditor && is_clojure(event.textEditor))
 			scheduleMatchPairs();
 	}, null, context.subscriptions);
 
 	vscode.workspace.onDidChangeTextDocument(event => {
-		j
+    if (is_clojure(activeEditor) && event.document === activeEditor.document)
 			scheduleRainbowBrackets();
 	}, null, context.subscriptions);
 
@@ -46,7 +48,7 @@ export function activate(context: vscode.ExtensionContext) {
 			cycleRainbow = configuration.get("cycleBracketColors");
 			misplacedType = vscode.window.createTextEditorDecorationType(configuration.get("misplacedBracketStyle") || { "color": "#fff", "backgroundColor": "#c33" });
 			matchedType = vscode.window.createTextEditorDecorationType(configuration.get("matchedBracketStyle") || {"backgroundColor": "#E0E0E0"});
-			vscode.workspace.getConfiguration().update('editor.matchBrackets', false, true);
+			vscode.workspace.getConfiguration(undefined, activeEditor.document.uri).update('editor.matchBrackets', false, true);
 		}
 	}
 
@@ -57,18 +59,18 @@ export function activate(context: vscode.ExtensionContext) {
 			clearTimeout(rainbowTimer);
 		if (matchTimer) // because updateRainbowBrackets triggers matchPairs
 			clearTimeout(matchTimer);
-		if (activeEditor && activeEditor.document.languageId === 'clojure')
+		if (is_clojure(activeEditor))
 			rainbowTimer = setTimeout(updateRainbowBrackets, 16);
 	}
 	function scheduleMatchPairs() {
 		if (matchTimer)
 			clearTimeout(matchTimer);
-		if (activeEditor && activeEditor.document.languageId === 'clojure')
+		if (is_clojure(activeEditor))
 			matchTimer = setTimeout(matchPairs, 16);
 	}
 
 	function updateRainbowBrackets() {
-		if (!activeEditor) return;
+		if (!is_clojure(activeEditor)) return;
 		reloadConfig();
 
 		const regexp     = /(\[|\]|\(|\)|\{|\}|\"|\\.|\;|\n)/g,
@@ -135,37 +137,83 @@ export function activate(context: vscode.ExtensionContext) {
 		matchPairs();
 	}
 
+	function matchBefore(doc: vscode.TextDocument, cursor: Position): Position {
+		if (cursor.character > 0) {
+			const cursor_before = cursor.translate(0,-1),
+						range_before  = new Range(cursor_before, cursor),
+						char_before   = doc.getText(range_before);
+			if (closing(char_before)/* || !opening(char_after)*/)
+				return bracketPairs.get(position_str(cursor_before));
+		}
+	}
+
+	function matchAfter(doc: vscode.TextDocument, cursor: Position): Position {
+		const cursor_after = cursor.translate(0,1);
+		if (cursor_after.line === cursor.line) {
+		  const range_after = new Range(cursor, cursor_after),
+					  char_after  = doc.getText(range_after);
+			if (opening(char_after)/* || !closing(char_before)*/)
+				return bracketPairs.get(position_str(cursor));
+		}
+	}
+
 	function matchPairs() {
-		if (!activeEditor) return;
+		if (!is_clojure(activeEditor)) return;
 		reloadConfig();
 		const matches = [],
 		      doc = activeEditor.document;
 		activeEditor.selections.forEach(selection => {
 			const cursor        = selection.active,
-						cursor_before = cursor.character > 0 ? cursor.translate(0,-1) : undefined,
-						range_before  = !!cursor_before ? new Range(cursor_before, cursor) : undefined,
-						char_before   = !!range_before ? doc.getText(range_before) : undefined,
-						cursor_after  = cursor.translate(0,1),
-						range_after   = cursor_after.line === cursor.line ? new Range(cursor, cursor_after) : undefined,
-						char_after    = !!range_after ? doc.getText(range_after) : undefined;
-
-			// check before cursor
-			if (closing(char_before)/* || !opening(char_after)*/) {
-				let match = bracketPairs.get(position_str(cursor_before));
-				if (match !== undefined) {
-					matches.push({range: range_before});
-					matches.push({range: new Range(match, match.translate(0,1))});
-				}
+						match_before  = matchBefore(doc, cursor),
+						match_after   = matchAfter(doc, cursor);
+			if (!!match_before) {
+				matches.push({range: new Range(cursor.translate(0,-1), cursor)});
+				matches.push({range: new Range(match_before, match_before.translate(0,1))});
 			}
-			// check after cursor
-			if (opening(char_after)/* || !closing(char_before)*/) {
-				let match = bracketPairs.get(position_str(cursor));
-				if (match !== undefined) {
-					matches.push({range: range_after});
-					matches.push({range: new Range(match, match.translate(0,1))});
-				}
+			if (!!match_after) {
+					matches.push({range: new Range(cursor, cursor.translate(0,1))});
+					matches.push({range: new Range(match_after, match_after.translate(0,1))});
 			}
 		});
 		activeEditor.setDecorations(matchedType, matches);	
 	}
+
+	function jumpToMatchingBracket() {
+		if (!is_clojure(activeEditor)) return;
+		reloadConfig();
+		const doc = activeEditor.document;
+
+		activeEditor.selections = activeEditor.selections.map(selection => {
+			const cursor        = selection.active,
+						match_before  = matchBefore(doc, cursor),
+						match_after   = matchAfter(doc, cursor);
+			if (!!match_before)
+				return new Selection(match_before, match_before);
+			else if (!!match_after)
+				return new Selection(match_after.translate(0,1), match_after.translate(0,1));
+			else
+				return selection;
+		});
+	}
+
+	function selectToMatchingBracket() {
+		if (!is_clojure(activeEditor)) return;
+		reloadConfig();
+		const doc = activeEditor.document;
+
+		activeEditor.selections = activeEditor.selections.map(selection => {
+			const cursor        = selection.active,
+						match_before  = matchBefore(doc, cursor),
+						match_after   = matchAfter(doc, cursor);
+			if (!!match_before)
+				return new Selection(cursor, match_before);
+			else if (!!match_after)
+				return new Selection(cursor, match_after.translate(0,1));
+			else
+				return selection;
+		});
+	}
+
+	vscode.commands.registerCommand("clojureWarrior.jumpToMatchingBracket", jumpToMatchingBracket);
+	vscode.commands.registerCommand("clojureWarrior.selectToMatchingBracket", selectToMatchingBracket);
 }
