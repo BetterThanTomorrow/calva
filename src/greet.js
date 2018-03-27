46d9ee226da7005155acc10129b6c1e5b26fa38b
Peter Strömberg <pez@pezius.com>
2018-03-27 14:11:14 +0200

    Fix embarrassing spelling error

--- src/greet.js
+++ src/greet.js
@@ -1,7 +1,7 @@
 function activationGreetings(chan) {
     chan.appendLine("Clojure 4 VS Code activated. Happy coding! ❤️");
     chan.appendLine("Please file any feature requests or bug reports here: https://github.com/PEZ/clojure4vscode/issues");
-    chan.appendLine("I will also respond to any @pez mentions in the #editors shannel of the Clojurians Slack: https://clojurians.slack.com/messages/editors/");
+    chan.appendLine("I will also respond to any @pez mentions in the #editors channel of the Clojurians Slack: https://clojurians.slack.com/messages/editors/");
     chan.appendLine("");
     chan.appendLine("Note: Autolinting is now diabled by default. You need to enable \"clojure4vscode.lintOnSave\" in your editor settings to use it. But first install Joker: https://github.com/candid82/joker")
     chan.appendLine("-".repeat(25));
