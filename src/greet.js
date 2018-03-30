715a479a3c8117d16f0c9a70c29a3c8f64ee0eb7
Peter Strömberg <pez@pezius.com>
2018-03-30 17:43:09 +0200

    Conditional autolinting greet message

--- src/greet.js
+++ src/greet.js
@@ -1,9 +1,14 @@
+const state = require('./state');
+
 function activationGreetings(chan) {
     chan.appendLine("Calva activated. Happy Clojure coding! ❤️");
     chan.appendLine("Please file any feature requests or bug reports here: https://github.com/PEZ/clojure4vscode/issues");
     chan.appendLine("I will also respond to any @pez mentions in the #editors channel of the Clojurians Slack: https://clojurians.slack.com/messages/editors/");
-    chan.appendLine("");
-    chan.appendLine("Note: Autolinting is now diabled by default. You need to enable \"clojure4vscode.lintOnSave\" in your editor settings to use it. But first install Joker: https://github.com/candid82/joker")
+
+    if (!state.config().lint) {
+        chan.appendLine("");
+        chan.appendLine("Note: Autolinting is diabled. You need to enable \"clojure4vscode.lintOnSave\" in your editor settings to use it. But first install Joker: https://github.com/candid82/joker")
+    }
     chan.appendLine("-".repeat(3));
 }
 
