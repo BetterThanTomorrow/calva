6da8f50583a08483f57c038580248932e7f9daa0
Peter Strömberg <pez@Peters-MacBook-Pro.local>
2018-03-31 13:51:15 +0200

    Format on save now working. Fixes #4

--- src/providers/formatter.js
+++ src/providers/formatter.js
@@ -7,51 +7,51 @@ const {
     getSession
 } = require('../utilities');
 
-module.exports = class DocumentFormattingEditProvider {
+class DocumentFormattingEditProvider {
     constructor() {
         this.state = state;
     }
 
-    provideDocumentFormattingEdits(document, options, token) {
+    provideDocumentFormattingEdits(document) {
         let current = state.deref(),
+            chan = current.get('outputChannel'),
             doc = getDocument(document);
 
         if (current.get('connected')) {
-            let client = null;
             return new Promise((resolve, reject) => {
-                client = repl.create().once('connect', () => {
+                let client = repl.create().once('connect', () => {
                     let msg = {
                         op: "format-code",
                         code: doc.getText(),
                         session: getSession(getFileType(doc))
                     };
-                    client.send(msg, function (results) {
+                    client.send(msg, (results) => {
                         let r = results[0];
                         if (r.status.indexOf("format-code-error") !== -1) {
-                            let chan = state.deref().get('outputChannel'),
-                                end = r.err.indexOf("]");
+                            let end = r.err.indexOf("]");
                             chan.appendLine("Unable to format file - error found");
                             if (end !== -1 && end > 0) {
                                 chan.appendLine(r.err.substring(0, end).replace("[", ""));
                             }
                             reject(null);
                         } else if (r.status.length === 1 && r.status.indexOf("done") !== -1) {
-                            const fullRange = new vscode.Range(doc.positionAt(0), doc.positionAt(doc.getText().length - 1))
-                            let formattedDocument = [vscode.TextEdit.replace(fullRange, r["formatted-code"])]
-                            resolve(formattedDocument);
+                            const fullRange = new vscode.Range(doc.positionAt(0), doc.positionAt(doc.getText().length))
+                            let reformat = vscode.TextEdit.replace(fullRange, r["formatted-code"]);
+                            chan.appendLine(r["formatted-code"])
+                            resolve([reformat]);
                         } else {
-                            console.log("UNHANDLED FORMAT ERROR");
-                            console.log(results);
+                            chan.appendLine("UNHANDLED FORMAT ERROR");
+                            chan.appendLine(results);
                             reject(null);
                         }
+                        client.end();
                     });
                 });
-            }).then((formatresult) => {
-                client.end();
-                return formatresult;
             });
         } else {
             return null;
         }
     }
-};
+}
+
+module.exports = DocumentFormattingEditProvider;
