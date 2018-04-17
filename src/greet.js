fcbcd9147aa084b09b15c1d68beaa57528948c0d
Peter Strömberg <pez@pezius.com>
2018-04-17 20:46:47 +0200

    Add notes about not auto eval, nor auto test

--- src/greet.js
+++ src/greet.js
@@ -5,7 +5,8 @@ function activationGreetings(chan) {
     chan.appendLine("Please file any feature requests or bug reports here: https://github.com/PEZ/clojure4vscode/issues");
     chan.appendLine("I will also respond to any @pez mentions in the #editors channel of the Clojurians Slack: https://clojurians.slack.com/messages/editors/");
     chan.appendLine("");
-    chan.appendLine("BIG FAT NOTE: All keyboard shortcuts have been changed to use prefix CTRL+ALT+V, this due to: https://github.com/PEZ/clojure4vscode/issues/9");
+    chan.appendLine("NOTE: Files are no longer automatically evaluated when opened. You will need to issue the evaluate file command at will instead.");
+    chan.appendLine("NOTE: The evaluate file command does not seem to work in shadow-cljs clojurescript repls. A workaround, that someetimes suffices, is to select all contents of the file and evaluate it.");
     chan.appendLine("");
 
     if (!state.config().lint) {
