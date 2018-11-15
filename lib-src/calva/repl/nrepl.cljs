ea378039f32674d763615c885154ffa53391b43e
Peter Strömberg <pez@pezius.com>
2018-11-15 10:27:59 +0100

    Log less of nrepl connects and disconnects

--- lib-src/calva/repl/nrepl.cljs
+++ lib-src/calva/repl/nrepl.cljs
@@ -16,19 +16,19 @@
   (let [{:keys [host port on-connect on-error on-end] :or {host "localhost"}} (js->clj options :keywordize-keys true)]
     (doto (net/createConnection #js {:host host :port port})
       (.once "connect" (fn []
-                         (js/console.log (str "Connected to " host ":" port))
+                         #_(js/console.log (str "Connected to " host ":" port))
 
                          (when on-connect
                            (on-connect))))
 
       (.once "end" (fn []
-                     (js/console.log (str "Disconnected from " host ":" port))
+                     #_(js/console.log (str "Disconnected from " host ":" port))
 
                      (when on-end
                        (on-end))))
 
       (.once "error" (fn [error]
-                       (js/console.log (str "Failed to connect to " host ":" port) error)
+                       #_(js/console.log (str "Failed to connect to " host ":" port) error)
 
                        (when on-error
                          (on-error error)))))))
