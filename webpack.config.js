f6103e0f6ec964be412076ab3bd9b165ca2fc669
Peter Strömberg <pez@pezius.com>
2019-04-02 23:05:43 +0200

    Explicitly don't emit files

--- webpack.config.js
+++ webpack.config.js
@@ -26,9 +26,9 @@ module.exports = {
       },
       {
         test: /\.(eot|woff|woff2|ttf|svg|png|jpg)$/,
-        loader: 'url-loader?limit=30000&name=[name]-[hash].[ext]',
+        loader: 'url-loader',
         options: {
-          outputPath: "out/assets"
+          emitFile: false
         }
       }
     ]
