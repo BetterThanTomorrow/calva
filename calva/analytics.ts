9752aa755316e754b04b25d55f05f6460cce8782
Peter Strömberg <pez@pezius.com>
2019-04-17 17:37:03 +0200

    Pick latest ua changes from master

--- calva/analytics.ts
+++ calva/analytics.ts
@@ -1,6 +1,11 @@
 import * as vscode from 'vscode';
 import * as UA from 'universal-analytics';
 import * as uuid from "uuid/v4";
+import * as os from 'os';
+
+// var debug = require('debug');
+// debug.log = console.info.bind(console);
+
 
 function userAllowsTelemetry(): boolean {
     const config = vscode.workspace.getConfiguration('telemetry');
@@ -9,18 +14,20 @@ function userAllowsTelemetry(): boolean {
 
 export default class Analytics {
     private visitor: UA.Visitor;
-    private extensionId: string;
+    private extension: vscode.Extension<any>;
     private extensionVersion: string;
     private store: vscode.Memento;
     private GA_ID = (process.env.CALVA_DEV_GA ? process.env.CALVA_DEV_GA : 'FUBAR-69796730-3').replace(/^FUBAR/, "UA");
 
     constructor(context: vscode.ExtensionContext) {
-        this.extensionId = "cospaia.clojure4vscode";
-        const extension = vscode.extensions.getExtension(this.extensionId)!;
-        this.extensionVersion = extension.packageJSON.version;
+        this.extension = vscode.extensions.getExtension("cospaia.clojure4vscode")!;
+        this.extensionVersion = this.extension.packageJSON.version;
         this.store = context.globalState;
 
         this.visitor = UA(this.GA_ID, this.userID());
+        this.visitor.set("cd1", this.extensionVersion);
+        this.visitor.set("cn", `calva-${this.extensionVersion}`);
+        this.visitor.set("ua", `Calva/${this.extensionVersion} (${os.platform()}; ${os.release()}; ${os.type}) VSCode/${vscode.version}`);
     }
 
     private userID(): string {
@@ -33,7 +40,7 @@ export default class Analytics {
             return this.store.get(KEY);
         }
     }
-    
+
     private getVisitor(): UA.Visitor | { pageview, event, screenview } {
         const noop = {
             send: function () {
@@ -50,16 +57,16 @@ export default class Analytics {
             }
         }
     }
-        
+
     logPath(path: string) {
         this.getVisitor().pageview(path).send();
     }
-        
+
     logView(view: string) {
-        this.getVisitor().screenview(view, "Calva", this.extensionVersion, this.extensionVersion).send();
+        this.getVisitor().screenview(view, "Calva", this.extensionVersion, this.extension.id).send();
     }
-    
-    logEvent(category: string, action: string, label?: string, value?:string ) {
+
+    logEvent(category: string, action: string, label?: string, value?: string) {
         this.getVisitor().event({ ec: category, ea: action, el: label, ev: value }).send();
     }
 }
