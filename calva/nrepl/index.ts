1cfbf27539e30a39ed7745f9df9833cf257e2f5b
Peter Strömberg <pez@pezius.com>
2019-06-10 18:57:07 +0200

    Use fipp for pprint

--- calva/nrepl/index.ts
+++ calva/nrepl/index.ts
@@ -195,8 +195,8 @@ export class NReplSession {
     eval(code: string, opts: { line?: number, column?: number, eval?: string, file?: string, stderr?: (x: string) => void, stdout?: (x: string) => void, pprint?: boolean } = {}) {
         const id = this.client.nextId,
             pprintOpts = opts.pprint ? {
-                "nrepl.middleware.print/print": "cider.nrepl.pprint/pprint",
-                "nrepl.middleware.print/options": {"length": 80}
+                "nrepl.middleware.print/print": "cider.nrepl.pprint/fipp-pprint",
+                "nrepl.middleware.print/options": {"length": 120}
             } : {};
 
         let evaluation = new NReplEvaluation(id, this, opts.stderr, opts.stdout, new Promise((resolve, reject) => {
