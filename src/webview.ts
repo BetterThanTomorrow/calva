e2d141603bcb0b6aea3d1d6a403746e195f197ab
cf <cf@infomantis.de>
2019-10-11 11:06:22 +0200

    Removed unused code.

--- src/webview.ts
+++ src/webview.ts
@@ -50,21 +50,6 @@ document.addEventListener("DOMContentLoaded", () => {
 })
 
 
-const motd = [
-    "Some said the world should be in Perl, \nSome said in Lisp.\nNow, having given both a whirl,\nI held with those who favored Perl.\nBut I fear we passed to men\nA disappointing founding myth.\nAnd should we write it all again,\nI'd end it with\nA close-paren. -Randall Munroe",
-    "I object to doing things that computers can do. -Olin Shivers",
-    "Will write code that writes code that writes code that writes code for money.",
-    "Anyone could learn Lisp in one day, except that if they already knew Fortran, it would take three days. -Marvin Minsky",
-    "Your Kitten of Death awaits. -Christopher Rhodes.",
-    "Syntactic sugar causes cancer of the semicolon. -Alan Perlis",
-    "If you have a procedure with ten parameters, you probably missed some. -Alan Perlis",
-    "Beware of the Turing tar-pit in which everything is possible but nothing of interest is easy. -Alan Perlis",
-    "If you give someone Fortran, he has Fortran. If you give someone Lisp, he has any language he pleases. -Guy L. Steele Jr",
-    "If you have more things than names, your design is broken- Stuart Halloway",
-    "Made with secret alien technology."
-]
-
-
 function makeTd(className: string, text: string) {
     let td = document.createElement("td");
     td.className = className;
@@ -499,4 +484,3 @@ window.onmessage = (msg) => {
     }
 }
 message.postMessage({ type: "init" });
-// document.querySelector("#motd").textContent = motd[Math.floor(Math.random() * motd.length)];
