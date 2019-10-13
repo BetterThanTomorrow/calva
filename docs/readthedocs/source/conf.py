5ec8f488fd6e0976a1d02ab36d91e6ade914900b
Peter Strömberg <pez@pezius.com>
2019-10-13 23:49:54 +0200

    Add python conf

--- docs/readthedocs/source/conf.py
+++ docs/readthedocs/source/conf.py
@@ -26,6 +26,11 @@ release = '2.0.49'
 
 
 # -- General configuration ---------------------------------------------------
+python:
+  version: 3.7
+  install:
+    - requirements: docs/readthedocs/source/requirements.txt
+
 import sphinx_rtd_theme
 
 master_doc = 'index'
