cb4703b6092d3e6a983ec7574d619a70595c2303
Peter Strömberg <pez@pezius.com>
2019-10-13 22:37:34 +0200

    Change theme

--- docs/readthedocs/source/conf.py
+++ docs/readthedocs/source/conf.py
@@ -26,13 +26,17 @@ release = '2.0.49'
 
 
 # -- General configuration ---------------------------------------------------
+import sphinx_rtd_theme
 
 master_doc = 'index'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['recommonmark']
+extensions = [
+    'recommonmark',
+    'sphinx_rtd_theme'
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -48,7 +52,13 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+# html_theme = 'alabaster'
+import os
+on_rtd = os.environ.get('READTHEDOCS') == 'True'
+if on_rtd:
+    html_theme = 'default'
+else:
+    html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
