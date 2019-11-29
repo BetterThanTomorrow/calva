5e7f1480601413296f24406748d0b70c497a6765
Kevin Stehn <git@kstehn.de>
2019-11-29 13:06:55 +0100

    Adding Integration test Needs real tests, also only usable through the vscode launcher

--- src/test/integration/suite/extension.test.ts
+++ src/test/integration/suite/extension.test.ts
@@ -1,5 +1,6 @@
 import * as assert from 'assert';
 import { after } from 'mocha';
+import {keywordize, unKeywordize} from '../../../nrepl/project-types';
 
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
@@ -14,5 +15,12 @@ suite('Extension Test Suite', () => {
   test('Sample test', () => {
     assert.equal(-1, [1, 2, 3].indexOf(5));
     assert.equal(-1, [1, 2, 3].indexOf(0));
+  });
+
+  test('Un/Keywordize Test', () => {
+    assert.equal(":test", keywordize("test"));
+    assert.equal(":foo", keywordize(":foo"));
+    assert.equal("test", unKeywordize("test"));
+    assert.equal("foo", unKeywordize(":foo"));
   });
 });
