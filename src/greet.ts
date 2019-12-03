464ef4aade5f3a8e1a22c24f97a4fb93004ae069
Peter Strömberg <pez@pezius.com>
2019-12-03 08:54:48 +0100

    Add sponsor @stigi

--- src/greet.ts
+++ src/greet.ts
@@ -1,8 +1,10 @@
 export function activationGreetings(chan) {
+    chan.appendLine("Did you know? There is Documentation: https://calva.readthedocs.io/");
     chan.appendLine("NOTE: Calva bundles the clj-kondo extension for your linting needs. Please see: https://calva.readthedocs.io/en/latest/linting.html");
     chan.appendLine("You are welcome to join the #calva-dev channel of the Clojurians Slack: https://clojurians.slack.com/messages/calva-dev/");
     chan.appendLine("Please file any feature requests or bug reports here: https://github.com/BetterThanTomorrow/calva/issues");
+    chan.appendLine("If you like Calva, please consider becoming a sponsor:");
+    chan.appendLine("  https://github.com/sponsors/PEZ ❤️");
     chan.appendLine("Happy Clojure(script) coding!");
-    chan.appendLine("If you like Calva, please consider becoming a sponsor:\n❤️ https://github.com/sponsors/PEZ ❤️");
     chan.appendLine("--");
 }
