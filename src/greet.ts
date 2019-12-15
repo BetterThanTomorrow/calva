a40b2f8d25d9db02aacd305a52001e739b7238b5
Peter Strömberg <pez@pezius.com>
2019-12-15 17:29:21 +0100

    Add sponsor @ITSecMedia
    [skip ci]

--- src/greet.ts
+++ src/greet.ts
@@ -1,10 +1,10 @@
 export function activationGreetings(chan) {
     chan.appendLine("NOTE: Calva bundles the clj-kondo extension for your linting needs. Please see: https://calva.readthedocs.io/en/latest/linting.html");
     chan.appendLine("Calva Documentation: https://calva.readthedocs.io/");
-    chan.appendLine("Please feel welcome to join the #calva-dev channel of the Clojurians Slack: https://clojurians.slack.com/messages/calva-dev/");
     chan.appendLine("Feature requests or bug reports here: https://github.com/BetterThanTomorrow/calva/issues");
+    chan.appendLine("You are welcome to join the #calva-dev channel of the Clojurians Slack: https://clojurians.slack.com/messages/calva-dev/");
     chan.appendLine("--");
-    chan.appendLine("If you like Calva, please see the wiki for how to contribute:");
+    chan.appendLine("If you like Calva, please consider how you can contribute:");
     chan.appendLine("  https://github.com/BetterThanTomorrow/calva/wiki/How-to-Contribute");
     chan.appendLine("Happy Clojure(script) coding! ❤️");
     chan.appendLine("--");
