1d7769e67410ceb97dfebf0c017b6be771f755c8
Nazarii Bardiuk <nazarii@bardiuk.com>
2020-01-20 05:18:36 +0000

    remove individual cljfmt settings

--- src/calva-fmt/src/config.ts
+++ src/calva-fmt/src/config.ts
@@ -7,12 +7,6 @@ function readConfiguration() {
     const cljfmtContent = state.readFile(state.resolvePath(workspaceConfig.get("configPath")));
     return {
         "format-as-you-type": workspaceConfig.get("formatAsYouType") as boolean,
-        "indentation?": workspaceConfig.get("indentation"),
-        "remove-surrounding-whitespace?": workspaceConfig.get("removeSurroundingWhitespace"),
-        "remove-trailing-whitespace?": workspaceConfig.get("removeTrailingWhitespace"),
-        "insert-missing-whitespace?": workspaceConfig.get("insertMissingWhitespace"),
-        "remove-consecutive-blank-lines?": workspaceConfig.get("removeConsecutiveBlankLines"),
-        "align-associative?": workspaceConfig.get("alignMapItems"),
         "cljfmt-string": cljfmtContent,
         "cljfmt-options": cljfmtOptions(cljfmtContent),
     };
