d3ac5697da9562820a4acb3a2c34caae5f783094
Brandon Ringe <brandon.ringe@gmail.com>
2020-02-07 15:37:38 -0800

    Use LoggingDebugger

--- src/calvaDebug.ts
+++ src/calvaDebug.ts
@@ -1,7 +1,12 @@
-import { DebugSession } from 'vscode-debugadapter';
+import { LoggingDebugSession } from 'vscode-debugadapter';
 
-export class CalvaDebugSession extends DebugSession {
+export class CalvaDebugSession extends LoggingDebugSession {
+    public constructor() {
+        super('calva-debug-logs.txt');
 
+        this.setDebuggerLinesStartAt1(false);
+        this.setDebuggerColumnsStartAt1(false);
+    }
 }
 
 CalvaDebugSession.run(CalvaDebugSession);
