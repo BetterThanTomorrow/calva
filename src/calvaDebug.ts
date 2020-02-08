6d57f4f4f1cfb5df55eaf9e15aa29d6122bc7d21
Brandon Ringe <brandon.ringe@gmail.com>
2020-02-07 16:30:11 -0800

    Add initializeRequest - WIP

--- src/calvaDebug.ts
+++ src/calvaDebug.ts
@@ -1,4 +1,5 @@
-import { LoggingDebugSession } from 'vscode-debugadapter';
+import { LoggingDebugSession, InitializedEvent } from 'vscode-debugadapter';
+import { DebugProtocol } from 'vscode-debugprotocol';
 
 export class CalvaDebugSession extends LoggingDebugSession {
     public constructor() {
@@ -6,6 +7,22 @@ export class CalvaDebugSession extends LoggingDebugSession {
 
         this.setDebuggerLinesStartAt1(false);
         this.setDebuggerColumnsStartAt1(false);
+    }
+
+    /**
+	 * The 'initialize' request is the first request called by the frontend
+	 * to interrogate the features the debug adapter provides.
+	 */
+    protected initializeRequest(response: DebugProtocol.InitializeResponse, args: DebugProtocol.InitializeRequestArguments): void {
+        // Build and return the capabilities of this debug adapter
+		response.body = response.body || {};
+
+        this.sendResponse(response);
+
+        // Since this debug adapter can accept configuration requests like 'setBreakpoint' at any time,
+		// we request them early by sending an 'initializeRequest' to the frontend.
+		// The frontend will end the configuration sequence by calling 'configurationDone' request.
+        this.sendEvent(new InitializedEvent());
     }
 }
 
