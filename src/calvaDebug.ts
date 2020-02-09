d7984c1a4198924aa661c0c12b37aeb459892859
Brandon Ringe <brandon.ringe@gmail.com>
2020-02-09 12:27:55 -0800

    Refactor most debug related code out of extension.ts

--- src/calvaDebug.ts
+++ src/calvaDebug.ts
@@ -1,9 +1,16 @@
 import { LoggingDebugSession, InitializedEvent, TerminatedEvent } from 'vscode-debugadapter';
 import { DebugProtocol } from 'vscode-debugprotocol';
-import { window } from 'vscode';
+import { window, DebugConfigurationProvider, WorkspaceFolder, DebugConfiguration, CancellationToken, ProviderResult, DebugAdapterDescriptorFactory, DebugAdapterDescriptor, DebugSession, DebugAdapterExecutable, DebugAdapterServer } from 'vscode';
 import * as util from './utilities';
+import * as Net from 'net';
 
-export class CalvaDebugSession extends LoggingDebugSession {
+const CALVA_DEBUG_CONFIGURATION: DebugConfiguration = {
+    type: 'clojure',
+    name: 'Calva Debug',
+    request: 'attach'
+};
+
+class CalvaDebugSession extends LoggingDebugSession {
     public constructor() {
         super('calva-debug-logs.txt');
     }
@@ -45,3 +52,55 @@ export class CalvaDebugSession extends LoggingDebugSession {
 }
 
 CalvaDebugSession.run(CalvaDebugSession);
+
+class CalvaDebugConfigurationProvider implements DebugConfigurationProvider {
+
+	/**
+	 * Massage a debug configuration just before a debug session is being launched,
+	 * e.g. add all missing attributes to the debug configuration.
+	 */
+	resolveDebugConfiguration(folder: WorkspaceFolder | undefined, config: DebugConfiguration, token?: CancellationToken): ProviderResult<DebugConfiguration> {
+
+		// If launch.json is missing or empty
+		if (!config.type && !config.request && !config.name) {
+			const editor = window.activeTextEditor;
+			if (editor && editor.document.languageId === 'clojure') {
+				config = {...config, CALVA_DEBUG_CONFIGURATION};
+			}
+		}
+
+		return config;
+	}
+}
+
+class CalvaDebugAdapterDescriptorFactory implements DebugAdapterDescriptorFactory {
+
+	private server?: Net.Server;
+
+	createDebugAdapterDescriptor(session: DebugSession, executable: DebugAdapterExecutable | undefined): ProviderResult<DebugAdapterDescriptor> {
+
+		if (!this.server) {
+			// start listening on a random port (0 means an arbitrary unused port will be used)
+			this.server = Net.createServer(socket => {
+				const session = new CalvaDebugSession();
+				session.setRunAsServer(true);
+				session.start(<NodeJS.ReadableStream>socket, socket);
+			}).listen(0);
+		}
+
+		// make VS Code connect to debug server
+		return new DebugAdapterServer(this.server.address().port);
+	}
+
+	dispose() {
+		if (this.server) {
+			this.server.close();
+		}
+	}
+}
+
+export {
+    CALVA_DEBUG_CONFIGURATION,
+    CalvaDebugConfigurationProvider,
+    CalvaDebugAdapterDescriptorFactory
+};
--- src/extension.ts
+++ src/extension.ts
@@ -23,9 +23,7 @@ import * as greetings from "./greet";
 import Analytics from './analytics';
 import * as open from 'open';
 import statusbar from './statusbar';
-import * as Net from 'net';
-import { CalvaDebugSession } from './calvaDebug';
-import { WorkspaceFolder, CancellationToken, DebugConfiguration, ProviderResult } from 'vscode';
+import { CalvaDebugConfigurationProvider, CalvaDebugAdapterDescriptorFactory, CALVA_DEBUG_CONFIGURATION } from './calvaDebug';
 
 function onDidSave(document) {
     let {
@@ -193,12 +191,11 @@ function activate(context: vscode.ExtensionContext) {
         statusbar.update();
     }));
 
-    // Debugger setup
-    const debuggerType = 'clojure';
+    // Clojure debugger setup
     const provider = new CalvaDebugConfigurationProvider();
-    context.subscriptions.push(vscode.debug.registerDebugConfigurationProvider(debuggerType, provider));
+    context.subscriptions.push(vscode.debug.registerDebugConfigurationProvider(CALVA_DEBUG_CONFIGURATION.type, provider));
     const factory = new CalvaDebugAdapterDescriptorFactory();
-    context.subscriptions.push(vscode.debug.registerDebugAdapterDescriptorFactory(debuggerType, factory));
+    context.subscriptions.push(vscode.debug.registerDebugAdapterDescriptorFactory(CALVA_DEBUG_CONFIGURATION.type, factory));
     if ('dispose' in factory) {
         context.subscriptions.push(factory);
     }
@@ -248,54 +245,5 @@ function deactivate() {
     jackIn.calvaJackout();
     paredit.deactivate()
 }
-
-class CalvaDebugConfigurationProvider implements vscode.DebugConfigurationProvider {
-
-	/**
-	 * Massage a debug configuration just before a debug session is being launched,
-	 * e.g. add all missing attributes to the debug configuration.
-	 */
-	resolveDebugConfiguration(folder: WorkspaceFolder | undefined, config: DebugConfiguration, token?: CancellationToken): ProviderResult<DebugConfiguration> {
-
-		// If launch.json is missing or empty
-		if (!config.type && !config.request && !config.name) {
-			const editor = vscode.window.activeTextEditor;
-			if (editor && editor.document.languageId === 'clojure') {
-				config.type = 'clojure';
-				config.name = 'Calva Debug';
-                config.request = 'attach';
-			}
-		}
-
-		return config;
-	}
-}
-
-class CalvaDebugAdapterDescriptorFactory implements vscode.DebugAdapterDescriptorFactory {
-
-	private server?: Net.Server;
-
-	createDebugAdapterDescriptor(session: vscode.DebugSession, executable: vscode.DebugAdapterExecutable | undefined): vscode.ProviderResult<vscode.DebugAdapterDescriptor> {
-
-		if (!this.server) {
-			// start listening on a random port (0 means an arbitrary unused port will be used)
-			this.server = Net.createServer(socket => {
-				const session = new CalvaDebugSession();
-				session.setRunAsServer(true);
-				session.start(<NodeJS.ReadableStream>socket, socket);
-			}).listen(0);
-		}
-
-		// make VS Code connect to debug server
-		return new vscode.DebugAdapterServer(this.server.address().port);
-	}
-
-	dispose() {
-		if (this.server) {
-			this.server.close();
-		}
-	}
-}
-
 
 export { activate, deactivate };
