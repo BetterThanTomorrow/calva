50ae4933b16163b6d9907b682e50c13f52193509
Peter <pez@pezius.com>
2020-02-22 14:53:06 +0100

    Add some test-data for grammar

---
+++ test-data/nom.clj
@@ -0,0 +1,15 @@
+(ns nom
+  (:gen-class))
+
+failure-expected-here-dont-freak-out
+
+This noming squirrel will cause compilation of this file to fail.
+
+ ,;;:;,
+   ;;;;;
+  ,:;;:;    ,'=.
+  ;:;:;' .=" ,'_\
+  ':;:;,/  ,__:=@
+   ';;:;  =./)_
+     `"=\_  )_"`
+          ``'"
