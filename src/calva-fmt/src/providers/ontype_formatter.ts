d5898ba7fc925ac1302c08d422f9be13d6bf11cf
Peter <pez@pezius.com>
2020-02-22 17:10:05 +0100

    Add line comment continuation
    Fixes #536

--- src/calva-fmt/src/providers/ontype_formatter.ts
+++ src/calva-fmt/src/providers/ontype_formatter.ts
@@ -1,20 +1,38 @@
 import * as vscode from 'vscode';
 import * as formatter from '../format';
 
-export class FormatOnTypeEditProvider {
-    async provideOnTypeFormattingEdits(document: vscode.TextDocument, position: vscode.Position, _ch, _options) {
-        if (vscode.workspace.getConfiguration("calva.fmt").get("formatAsYouType")) {
-            if (vscode.workspace.getConfiguration("calva.fmt").get("newIndentEngine")) {
-                formatter.indentPosition(position, document);
-            } else {
-                try {
-                    formatter.formatPosition(vscode.window.activeTextEditor, true);
-                } catch (e) {
-                    formatter.indentPosition(position, document);
-                }
+function continueComment(editor: vscode.TextEditor, document: vscode.TextDocument, position: vscode.Position): Thenable<boolean> {
+    const prevLineRange = new vscode.Range(position.with(position.line - 1, 0), position.with(position.line)),
+        prevLineText: string = document.getText(prevLineRange),
+        match = prevLineText.match(/^[ \t]*;;[ \t]*/);
+    console.log(match);
+    if (match) {
+        const [commentStart] = match;
+        return editor.edit(edits => edits.insert(position.with(position.line, 0), commentStart), { undoStopAfter: false, undoStopBefore: true });
+    } else {
+        return new Promise((resolve, _reject) => {
+            resolve(true);
+        });
+    }
+}
 
+export class FormatOnTypeEditProvider {
+    async provideOnTypeFormattingEdits(document: vscode.TextDocument, _position: vscode.Position, _ch, _options) {
+        const editor = vscode.window.activeTextEditor;
+        continueComment(editor, document, editor.selection.active).then(() => {
+            const pos = editor.selection.active;
+            if (vscode.workspace.getConfiguration("calva.fmt").get("formatAsYouType")) {
+                if (vscode.workspace.getConfiguration("calva.fmt").get("newIndentEngine")) {
+                    return formatter.indentPosition(pos, document);
+                } else {
+                    try {
+                        return formatter.formatPosition(editor, true);
+                    } catch (e) {
+                        return formatter.indentPosition(pos, document);
+                    }
+                }
             }
-        }
+        });
         return null;
     }
 }
