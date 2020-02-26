6deb4aa4f960e09445eb57d13f044a2e29772ebc
Peter Strömberg <pez@pezius.com>
2020-02-26 09:22:55 +0100

    Using clojure-lsp from repo releases

--- src/cursor-doc/clojure-lexer.ts
+++ src/cursor-doc/clojure-lexer.ts
@@ -82,7 +82,6 @@ toplevel.terminal(/#[^\(\)\[\]\{\}"_@~\s,]+/, (_l, _m) => ({ type: "reader" }));
 // TODO: Figure out why we can't allow `'` in the symbol name
 toplevel.terminal(/(['`~#^@]\s*)*([^_()[\]\{\}#,~@'`^\"\s:;][^()[\]\{\},~@`'^\"\s;]*)/, (l, m) => ({ type: "id" }))
 
-
 toplevel.terminal(/./, (l, m) => ({ type: "junk" }))
 
 /** This is inside-string string grammar. It spits out 'close' once it is time to switch back to the 'toplevel' grammar,
