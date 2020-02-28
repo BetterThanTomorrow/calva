33d32749c18bbf329ce7dcd332f739cfa80ef7ac
Peter <pez@pezius.com>
2020-02-28 21:59:11 +0100

    Add tests for tokenisation of lists, strings, etc

--- src/extension-test/unit/cursor-doc/clojure-lexer-test.ts
+++ src/extension-test/unit/cursor-doc/clojure-lexer-test.ts
@@ -10,20 +10,135 @@ describe('Scanner', () => {
         scanner = new Scanner(MAX_LINE_LENGTH);
     });
 
-    describe('tokenize', () => {
-        it('symbol', () => {
+    describe('simple', () => {
+        it('tokenizes symbol', () => {
             const tokens = scanner.processLine('foo');
             expect(tokens[0].type).equals('id');
             expect(tokens[0].raw).equals('foo');
         });
-        it('keyword', () => {
+        it('tokenizes whitespace', () => {
+            const tokens = scanner.processLine('foo   bar');
+            expect(tokens[1].type).equals('ws');
+            expect(tokens[1].raw).equals('   ');
+        });
+        it('tokenizes keyword', () => {
             const tokens = scanner.processLine(':foo');
             expect(tokens[0].type).equals('kw');
             expect(tokens[0].raw).equals(':foo');
         });
-        it('string', () => {
-            const tokens = scanner.processLine('"foo"').slice(0,3);
-            expect(tokens.map(t => { return [t.type, t.raw] })).deep.equals([['open', '"'], ['str-inside', 'foo'], ['close', '"']]);
+        it('tokenizes literal character', () => {
+            const tokens = scanner.processLine('\\a');
+            expect(tokens[0].type).equals('lit');
+            expect(tokens[0].raw).equals('\\a');
+        });
+        it('tokenizes literal named character', () => {
+            const tokens = scanner.processLine('\\space');
+            expect(tokens[0].type).equals('lit');
+            expect(tokens[0].raw).equals('\\space');
+        });
+        it('tokenizes line comments', () => {
+            const tokens = scanner.processLine('; foo');
+            expect(tokens[0].type).equals('comment');
+            expect(tokens[0].raw).equals('; foo');
+        });
+        it('tokenizes ignores', () => {
+            const tokens = scanner.processLine('#_foo');
+            expect(tokens[0].type).equals('ignore');
+            expect(tokens[0].raw).equals('#_');
+            expect(tokens[1].type).equals('id');
+            expect(tokens[1].raw).equals('foo');
+        });
+    });
+    describe('lists', () => {
+        it('tokenizes list', () => {
+            const tokens = scanner.processLine('(foo)');
+            expect(tokens[0].type).equals('open');
+            expect(tokens[0].raw).equals('(');
+            expect(tokens[1].type).equals('id');
+            expect(tokens[1].raw).equals('foo');
+            expect(tokens[2].type).equals('close');
+            expect(tokens[2].raw).equals(')');
+        });
+        it('tokenizes vector', () => {
+            const tokens = scanner.processLine('[foo]');
+            expect(tokens[0].type).equals('open');
+            expect(tokens[0].raw).equals('[');
+            expect(tokens[1].type).equals('id');
+            expect(tokens[1].raw).equals('foo');
+            expect(tokens[2].type).equals('close');
+            expect(tokens[2].raw).equals(']');
+        });
+        it('tokenizes map', () => {
+            const tokens = scanner.processLine('{:foo bar}');
+            expect(tokens[0].type).equals('open');
+            expect(tokens[0].raw).equals('{');
+            expect(tokens[1].type).equals('kw');
+            expect(tokens[1].raw).equals(':foo');
+            expect(tokens[2].type).equals('ws');
+            expect(tokens[2].raw).equals(' ');
+            expect(tokens[3].type).equals('id');
+            expect(tokens[3].raw).equals('bar');
+            expect(tokens[4].type).equals('close');
+            expect(tokens[4].raw).equals('}');
+        });
+        it('tokenizes set', () => {
+            const tokens = scanner.processLine('#{:foo :bar}');
+            expect(tokens[0].type).equals('open');
+            expect(tokens[0].raw).equals('#{');
+            expect(tokens[1].type).equals('kw');
+            expect(tokens[1].raw).equals(':foo');
+            expect(tokens[2].type).equals('ws');
+            expect(tokens[2].raw).equals(' ');
+            expect(tokens[3].type).equals('kw');
+            expect(tokens[3].raw).equals(':bar');
+            expect(tokens[4].type).equals('close');
+            expect(tokens[4].raw).equals('}');
+        });
+        it('tokenizes string', () => {
+            const tokens = scanner.processLine('"foo"');
+            expect(tokens[0].type).equals('open');
+            expect(tokens[0].raw).equals('"');
+            expect(tokens[1].type).equals('str-inside');
+            expect(tokens[1].raw).equals('foo');
+            expect(tokens[2].type).equals('close');
+            expect(tokens[2].raw).equals('"');
+        });
+        it('tokenizes regex', () => {
+            const tokens = scanner.processLine('#"foo"');
+            expect(tokens[0].type).equals('open');
+            expect(tokens[0].raw).equals('#"');
+            expect(tokens[1].type).equals('str-inside');
+            expect(tokens[1].raw).equals('foo');
+            expect(tokens[2].type).equals('close');
+            expect(tokens[2].raw).equals('"');
+        });
+    });
+    describe('strings', () => {
+        it('tokenizes words in strings', () => {
+            const tokens = scanner.processLine('"(foo :bar)"');
+            expect(tokens[0].type).equals('open');
+            expect(tokens[0].raw).equals('"');
+            expect(tokens[1].type).equals('str-inside');
+            expect(tokens[1].raw).equals('(foo');
+            expect(tokens[2].type).equals('ws');
+            expect(tokens[2].raw).equals(' ');
+            expect(tokens[3].type).equals('str-inside');
+            expect(tokens[3].raw).equals(':bar)');
+            expect(tokens[4].type).equals('close');
+            expect(tokens[4].raw).equals('"');
+        });
+        it('tokenizes newlines in strings', () => {
+            const tokens = scanner.processLine('"foo\nbar"');
+            expect(tokens[0].type).equals('open');
+            expect(tokens[0].raw).equals('"');
+            expect(tokens[1].type).equals('str-inside');
+            expect(tokens[1].raw).equals('foo');
+            expect(tokens[2].type).equals('ws');
+            expect(tokens[2].raw).equals('\n');
+            expect(tokens[3].type).equals('str-inside');
+            expect(tokens[3].raw).equals('bar');
+            expect(tokens[4].type).equals('close');
+            expect(tokens[4].raw).equals('"');
         });
     });
     describe('max line length', () => {
@@ -32,6 +147,26 @@ describe('Scanner', () => {
                 tokens = scanner.processLine(longLine);
             expect(tokens[0].type).equals('too-long-line');
             expect(tokens[0].raw).equals(longLine);
+        });
+    });
+    describe('issues', () => {
+        it('literal quotes - #566', () => {
+            const tokens = scanner.processLine("\\' foo");
+            expect(tokens[0].type).equals('lit');
+            expect(tokens[0].raw).equals("\\'");
+            expect(tokens[1].type).equals('ws');
+            expect(tokens[1].raw).equals(" ");
+            expect(tokens[2].type).equals('id');
+            expect(tokens[2].raw).equals("foo");
+        });
+        it('symbols ending in =? - #566', () => {
+            const tokens = scanner.processLine("foo=? foo");
+            expect(tokens[0].type).equals('id');
+            expect(tokens[0].raw).equals("foo=?");
+            expect(tokens[1].type).equals('ws');
+            expect(tokens[1].raw).equals(" ");
+            expect(tokens[2].type).equals('id');
+            expect(tokens[2].raw).equals("foo");
         });
     });
 });
