b9994b0d26bad5699cbaa1d2ea4f4fd72228faa0
Peter <pez@pezius.com>
2020-03-01 14:45:52 +0100

    Make lexer and token cursor handle reader tags
    Fixes #570

--- test-data/test2.clj
+++ test-data/test2.clj
@@ -1,5 +1,13 @@
-#foo  @~#(:bar)
-#foo 
-:bar
-\' [] abc
-\a [] "a"
+(comment
+  
+  #foo :bar
+  (foo #foo (foo :bar))
+  'abc #foo  @~#(foo :bar) \space
+  \' {:foo (#foo '[1 2 3])} 'abc
+  #foo
+   'bar
+  #foo
+   #{:foo (#foo '[1 2 3])}
+  \a [] "a"
+
+  )
