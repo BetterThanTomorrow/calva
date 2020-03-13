73261749d750362aeeb6b3b78d67ef3e8197930b
Peter Strömberg <pez@pezius.com>
2020-03-13 10:40:48 +0100

    Var quoted symbols are not readers
    Fixes #584

--- src/cursor-doc/clojure-lexer.ts
+++ src/cursor-doc/clojure-lexer.ts
@@ -76,7 +76,7 @@ toplevel.terminal(/(['`~#]\s*)*([-+]?[0-9]+(\.[0-9]+)?([eE][-+]?[0-9]+)?)/, (l, 
 toplevel.terminal(/(#[^\(\)\[\]\{\}"_@~\s,]+[\s,]*)*(['`~^]\s*)*(:[^()[\]\{\},~@`^\"\s;]*)/, (l, m) => ({ type: "kw" }))
 
 // data readers
-toplevel.terminal(/#[^\(\)\[\]\{\}"_@~\s,]+/, (_l, _m) => ({ type: "reader" }));
+toplevel.terminal(/#[^\(\)\[\]\{\}'"_@~\s,]+/, (_l, _m) => ({ type: "reader" }));
 
 // symbols, about anything goes!
 toplevel.terminal(/(['`~#^@]\s*)*([^_()[\]\{\}#,~@'`^\"\s:;][^()[\]\{\},~@`^\"\s;]*)/, (l, m) => ({ type: "id" }))
