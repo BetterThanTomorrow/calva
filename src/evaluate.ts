431fa8fb91582022df91f6fc0788100e6bd69c3d
Brandon Ringe <brandon.ringe@gmail.com>
2020-08-05 11:20:00 -0700

    Add in-ns call in front of user code to be evaluated so it counts as one evaluation

--- src/evaluate.ts
+++ src/evaluate.ts
@@ -56,9 +56,9 @@ async function evaluateCode(code: string, options, selection?: vscode.Selection)
     if (code.length > 0) {
         let err: string[] = [], out: string[] = [];
 
-        await session.eval("(in-ns '" + ns + ")", session.client.ns).value;
+        const codeWithInNsCall = `(do (in-ns '${ns}) ${code})`;
 
-        let context: NReplEvaluation = session.eval(code, ns, {
+        let context: NReplEvaluation = session.eval(codeWithInNsCall, ns, {
             file: filePath,
             line: line + 1,
             column: column + 1,
