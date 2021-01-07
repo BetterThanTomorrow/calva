e063d19d8b844bb0864110d5d372e6bd899782e5
Brandon Ringe <brandon.ringe@gmail.com>
2021-01-07 09:38:38 -0800

    Update clojure-lsp version to 2021.01.07-12.28.44

--- download-lsp.js
+++ download-lsp.js
@@ -1,7 +1,7 @@
 const { createWriteStream } = require('fs');
 const { https } = require('follow-redirects');
 
-const clojureLspVersion = '2021.01.06-03.00.28';
+const clojureLspVersion = '2021.01.07-12.28.44';
 const fileName = "./clojure-lsp.jar"
 
 const file = createWriteStream(fileName);
