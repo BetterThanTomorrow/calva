c4f564bf8c592cd350750d96a81e27cb2da42db9
Brandon Ringe <brandon.ringe@gmail.com>
2021-01-13 18:05:01 -0800

    Use lsp to get debugger decoration ranges - WIP

--- src/debugger/decorations.ts
+++ src/debugger/decorations.ts
@@ -4,6 +4,10 @@ import * as docMirror from '../doc-mirror';
 import { NReplSession } from '../nrepl';
 const { parseEdn } = require('../../out/cljs-lib/cljs-lib');
 import * as state from '../state';
+import { LanguageClient } from 'vscode-languageclient';
+import { Position, Location, DocumentSymbol } from 'vscode-languageserver-protocol';
+import lsp from '../lsp';
+import * as _ from 'lodash';
 
 let enabled = false;
 let decorationsSession: NReplSession = null;
@@ -39,6 +43,28 @@ async function getLintAnalysis(session: NReplSession, documentText: string): Pro
     const resEdn = await session.eval(code, 'user').value;
     await setPrintLength(session, printLength);
     return parseEdn(resEdn);
+}
+
+async function getReferences(lspClient: LanguageClient, uri: string, position: Position): Promise<Location[] | null> {
+    const result: Location[] = await lspClient.sendRequest('textDocument/references', {
+        textDocument: {
+            uri,
+        },
+        position,
+        context: {
+            includeDeclaration: true
+        }
+    });
+    return result;
+}
+
+async function getDocumentSymbols(lspClient: LanguageClient, uri: string): Promise<DocumentSymbol[]> {
+    const result: DocumentSymbol[] = await lspClient.sendRequest('textDocument/documentSymbol', {
+        textDocument: {
+            uri
+        }
+    });
+    return result;
 }
 
 function getVarDefinitionRanges(definitions: any[], document: vscode.TextDocument): [number, number][] {
@@ -80,19 +106,38 @@ async function updateDecorations(decorationsSession: NReplSession) {
 
             // Get editor ranges of instrumented var definitions and usages
             const lintAnalysis = await getLintAnalysis(decorationsSession, document.getText());
-            const instrumentedVarDefs = lintAnalysis['var-definitions'].filter(varInfo => instrumentedDefsInEditor.includes(varInfo.name));
-            const instrumentedVarDefRanges = getVarDefinitionRanges(instrumentedVarDefs, document);
-            const instrumentedVarUsages = lintAnalysis['var-usages'].filter(varInfo => {
-                const instrumentedDefsInVarNs = instrumentedDefs.list.filter(l => l[0] === varInfo.to)[0]?.slice(1) || [];
-                return instrumentedDefsInVarNs.includes(varInfo.name);
-            });
-            const instrumentedVarUsageRanges = getVarUsageRanges(instrumentedVarUsages, document);
 
-            const decorations = [...instrumentedVarDefRanges, ...instrumentedVarUsageRanges].map(([startOffset, endOffset]) => {
-                return {
-                    range: new vscode.Range(document.positionAt(startOffset), document.positionAt(endOffset)),
-                    hoverMessage: 'Instrumented for debugging'
+            const documentUri = document.uri.toString();
+            const lspClient = state.deref().get(lsp.LSP_CLIENT_KEY);
+            const documentSymbols = await getDocumentSymbols(lspClient, documentUri);
+            const instrumentedSymbolRanges = documentSymbols[0].children.filter(s => instrumentedDefsInEditor.includes(s.name)).map(s => s.range);
+            const instrumentedSymbolReferences = _.flatten(await Promise.all(instrumentedSymbolRanges.map(range => {
+                const position = {
+                    line: range.start.line,
+                    character: range.start.character
                 };
+                return getReferences(lspClient, documentUri, position);
+            })));
+            const instrumentedSymbolReferenceRanges = instrumentedSymbolReferences.filter(ref => ref.uri === documentUri).map(ref => ref.range);
+
+            // const instrumentedVarDefs = lintAnalysis['var-definitions'].filter(varInfo => instrumentedDefsInEditor.includes(varInfo.name));
+            // const instrumentedVarDefRanges = getVarDefinitionRanges(instrumentedVarDefs, document);
+            // const instrumentedVarUsages = lintAnalysis['var-usages'].filter(varInfo => {
+            //     const instrumentedDefsInVarNs = instrumentedDefs.list.filter(l => l[0] === varInfo.to)[0]?.slice(1) || [];
+            //     return instrumentedDefsInVarNs.includes(varInfo.name);
+            // });
+            //const instrumentedVarUsageRanges = getVarUsageRanges(instrumentedVarUsages, document);
+
+            // const decorations = [...instrumentedVarDefRanges, ...instrumentedVarUsageRanges].map(([startOffset, endOffset]) => {
+            //     return {
+            //         range: new vscode.Range(document.positionAt(startOffset), document.positionAt(endOffset)),
+            //         hoverMessage: 'Instrumented for debugging'
+            //     };
+            // });
+
+
+            const decorations = [...instrumentedSymbolRanges, ...instrumentedSymbolReferenceRanges].map(range => {
+                return new vscode.Range(range.start.line, range.start.character, range.end.line, range.end.character);
             });
             activeEditor.setDecorations(instrumentedFunctionDecorationType, decorations);
         } else {
