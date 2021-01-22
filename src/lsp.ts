21ff4dc92857ed50ad60bed94961c38c7228c465
Arne Brasseur <arne@arnebrasseur.net>
2021-01-22 08:30:05 +0100

    Add a configuration option to disable diagnostics
    First draft, input welcome. This seems to work, but existing annotations require
    a restart before they disappear.
    
    Closes #980

--- src/lsp.ts
+++ src/lsp.ts
@@ -29,6 +29,9 @@ function createClient(jarPath: string): LanguageClient {
         },
         middleware: {
             handleDiagnostics(uri, diagnostics, next) {
+                if (!state.config().displayDiagnostics) {
+                    return;
+                }
                 if (uri.path.endsWith(config.REPL_FILE_EXT)) {
                     return;
                 }
