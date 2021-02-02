5612a19592c8fa0a6c215437e29accc55b1410d4
Brandon Ringe <brandon.ringe@gmail.com>
2021-02-01 18:51:53 -0800

    Check content under Unreleased before updateing changelog

--- update-changelog.clj
+++ update-changelog.clj
@@ -7,5 +7,14 @@
 (let [changelog-filename "CHANGELOG.md"
       changelog-text (slurp changelog-filename)
       unreleased-header-re #"\[Unreleased\]"
+      content-after-unreleased (-> changelog-text
+                                   (str/split unreleased-header-re)
+                                   (nth 1)
+                                   (str/split #"##")
+                                   (nth 0)
+                                   str/trim)
       new-text (str/replace-first changelog-text unreleased-header-re "[Unreleased]\n\n## [MY NEW HEADER]")]
-  (spit changelog-filename new-text))
+  (if (empty? content-after-unreleased)
+    (throw (Exception. "There are no items to be released in the changelog. Update the changelog before releasing."))
+    content-after-unreleased)
+  #_(spit changelog-filename new-text))
