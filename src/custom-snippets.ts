c92b105f70bf4e8f4482df2ae783590e89dbe4aa
Peter Strömberg <pez@pezius.com>
2021-02-03 23:21:57 +0100

    Custom snippets use current repl by default

--- src/custom-snippets.ts
+++ src/custom-snippets.ts
@@ -4,56 +4,60 @@ import * as state from './state';
 import * as util from './utilities';
 import * as namespace from './namespace';
 import { customREPLCommandSnippet, evaluateInOutputWindow } from './evaluate';
+import { forEach } from 'lodash';
 
 export async function evaluateCustomCommandSnippetCommand(): Promise<void> {
-    let pickCounter = 1;
+    const editor = vscode.window.activeTextEditor;
+    const currentLine = editor.selection.active.line;
+    const currentColumn = editor.selection.active.character;
+    const currentFilename = editor.document.fileName;
+    let pickCounter = 0;
     let configErrors: { "name": string; "keys": string[]; }[] = [];
     const snippets = state.config().customREPLCommandSnippets as customREPLCommandSnippet[];
-    const snippetPicks = _.map(snippets, (c: customREPLCommandSnippet) => {
-        const undefs = ["name", "snippet", "repl"].filter(k => {
+    const snippetsDict = {};
+    const snippetsMenuItems: string[] = [];
+    snippets.forEach((c: customREPLCommandSnippet) => {
+        const undefs = ["name", "snippet"].filter(k => {
             return !c[k];
         });
         if (undefs.length > 0) {
             configErrors.push({ "name": c.name, "keys": undefs });
         }
-        return `${pickCounter++}: ${c.name} (${c.repl})`;
+        const entry = { ...c };
+        const editorNS = editor && editor.document && editor.document.languageId === 'clojure' ? namespace.getNamespace(editor.document) : undefined;
+        entry.ns = c.ns ? c.ns : editorNS;
+        const editorRepl = editor && editor.document && editor.document.languageId === 'clojure' ? namespace.getREPLSessionType() : "clj";
+        entry.repl = c.repl ? c.repl : editorRepl;
+        pickCounter++;
+        const prefix = c.key !== undefined ? c.key : pickCounter;
+        const item = `${prefix}: ${entry.name} (${entry.repl})`;
+        snippetsMenuItems.push(item);
+        snippetsDict[item] = entry;
     });
-    const snippetsDict = {};
-    pickCounter = 1;
 
     if (configErrors.length > 0) {
         vscode.window.showErrorMessage("Errors found in the `calva.customREPLCommandSnippets` setting. Values missing for: " + JSON.stringify(configErrors), "OK");
         return;
     }
-    snippets.forEach((c: customREPLCommandSnippet) => {
-        snippetsDict[`${pickCounter++}: ${c.name} (${c.repl})`] = c;
-    });
 
     if (snippets && snippets.length > 0) {
         try {
             const pick = await util.quickPickSingle({
-                values: snippetPicks,
+                values: snippetsMenuItems,
                 placeHolder: "Choose a command to run at the REPL",
                 saveAs: "runCustomREPLCommand"
             });
             if (pick && snippetsDict[pick] && snippetsDict[pick].snippet) {
-                const editor = vscode.window.activeTextEditor;
-                const currentLine = editor.selection.active.line;
-                const currentColumn = editor.selection.active.character;
-                const currentFilename = editor.document.fileName;
-                const editorNS = editor && editor.document && editor.document.languageId === 'clojure' ? namespace.getNamespace(editor.document) : undefined;
-                const ns = snippetsDict[pick].ns ? snippetsDict[pick].ns : editorNS;
-                const repl = snippetsDict[pick].repl ? snippetsDict[pick].repl : "clj";
                 const command = snippetsDict[pick].snippet.
                     replace("$line", currentLine).
                     replace("$column", currentColumn).
                     replace("$file", currentFilename).
-                    replace("$ns", ns).
+                    replace("$ns", snippetsDict[pick].ns).
                     replace("$current-form", util.currentFormText(editor, false)).
                     replace("$top-level-form", util.currentFormText(editor, true)).
                     replace("$current-fn", util.currentFunction(editor)).
                     replace("$top-level-defined-symbol", util.currentTopLevelFunction(editor));
-                await evaluateInOutputWindow(command, repl ? repl : "clj", ns);
+                await evaluateInOutputWindow(command, snippetsDict[pick].repl, snippetsDict[pick].ns);
             }
         } catch (e) {
             console.error(e);
