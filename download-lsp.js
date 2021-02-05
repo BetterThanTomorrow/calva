bda68bb485aa392f0662d4d24ad96dd0716053a2
Peter Strömberg <pez@pezius.com>
2021-02-05 16:22:05 +0100

    Upgrade clojure-lsp to 2021.02.05-03.05.34

--- download-lsp.js
+++ download-lsp.js
@@ -1,7 +1,7 @@
 const { createWriteStream } = require('fs');
 const { https } = require('follow-redirects');
 
-const clojureLspVersion = '2021.02.02-14.02.23';
+const clojureLspVersion = '2021.02.05-03.05.34';
 const fileName = "./clojure-lsp.jar"
 
 const file = createWriteStream(fileName);
