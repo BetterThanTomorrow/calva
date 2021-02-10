fc3f4206f528f8926d3bc146d643402149169bcb
Brandon Ringe <brandon.ringe@gmail.com>
2021-02-10 09:03:01 -0800

    Start clojure-lsp with cljs code

--- src/cljs-lib/src/calva/lsp.cljs
+++ src/cljs-lib/src/calva/lsp.cljs
@@ -1,13 +1,15 @@
 (ns calva.lsp
   (:require ["vscode" :as vscode]
             ["vscode-languageclient" :refer [LanguageClient]]
-            ["path" :as path]))
+            ["path" :as path]
+            [cljs.core.async :refer [go]]
+            [cljs.core.async.interop :refer-macros [<p!]]))
 
 (defn create-client [jarPath]
   (let [server-options {:run {:command "java"
-                             :args ["-jar" jarPath]}
-                       :debug {:command "java"
-                               :args ["-jar" jarPath]}}
+                              :args ["-jar" jarPath]}
+                        :debug {:command "java"
+                                :args ["-jar" jarPath]}}
         file-system-watcher (.. vscode -workspace (createFileSystemWatcher "**/.clientrc"))
         client-options {:documentSelector [{:scheme "file" :language "clojure"}]
                         :synchronize {:configurationSection "clojure-lsp"
@@ -19,12 +21,18 @@
                          "document-range-formatting?" false
                          "keep-require-at-start?" true}}]
     (LanguageClient. "clojure" "Clojure Language Client"
-                     server-options client-options)))
+                     (clj->js server-options) 
+                     (clj->js client-options))))
 
 ;; TODO: Find out what the type hint ^js does here
 (defn activate [^js context]
-  (let [jarPath (. path join (. context -extensionPath) "clojure-lsp.jar")]
-    (js/console.log "jarPath:" jarPath)))
+  (let [jar-path (. path join (. context -extensionPath) "clojure-lsp.jar")
+        client (create-client jar-path)]
+    (js/console.log "client:" client)
+    (.start client)
+    (go
+      (<p! (.onReady client))
+      (js/console.log "Client is ready!"))))
 
 (comment
   (LanguageClient. "clojure" "Clojure Language Client" {} {})
@@ -32,7 +40,14 @@
 
   (.. vscode -workspace (createFileSystemWatcher "**/.clientrc"))
 
-  (create-client "some-path")
-  
+  (js/console.log (create-client "some-path"))
+
+  (js/console.log (clj->js {:run {:command "java"
+                                  :args ["-jar" "jarPath"]}
+                            :debug {:command "java"
+                                    :args ["-jar" "jarPath"]}}))
+
+
+  (. state config)
   ;; Example interop
   (.. vscode -window (showInformationMessage "hello")))
