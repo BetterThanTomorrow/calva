2bf0d603c43f0f6c35bb4c85350cccd59319753b
Brandon Ringe <brandon.ringe@gmail.com>
2021-02-22 14:03:26 -0800

    Add handler for toggling references code lens to cljs lsp

--- src/cljs-lib/src/calva/lsp.cljs
+++ src/cljs-lib/src/calva/lsp.cljs
@@ -109,7 +109,7 @@
      :category "calva.diagnostics"}})
 
 (defn register-lsp-command
-  [client {:keys [name extra-param-fn category]}]
+  [^js client {:keys [name extra-param-fn category]}]
   (let [category (or category "calva.refactor")
         calva-command-name (str category "."
                                 (str/replace name #"-[a-z]"
@@ -150,6 +150,36 @@
           (register-lsp-command client command))
         lsp-commands))
 
+(defn handle-toggle-references-code-lens
+  [^js event]
+  (when (.. event (affectsConfiguration "calva.referencesCodeLens.enabled"))
+    (let [visible-editors (filter 
+                           (fn [editor]
+                             (= (.. editor -document -uri -scheme) "file"))
+                           (.. vscode -window -visibleTextEditors))]
+      ;; Hacky solution for triggering codeLens refresh
+      ;; Could not find a better way, aside from possibly changes to clojure-lsp
+      ;; https://github.com/microsoft/vscode-languageserver-node/issues/705
+      (run! (fn [editor]
+              (js/console.log "adding edits for:" (.. editor -document -uri toString))
+              (let [edit1 ^js (new (.. vscode -WorkspaceEdit))
+                    _ (.. edit1 (insert (.. editor -document -uri)
+                                        (new (.. vscode -Position) 0 0)
+                                        "\n"))
+                    edit2 ^js (new (.. vscode -WorkspaceEdit))
+                    _ (.. edit2 (delete (.. editor -document -uri)
+                                        (new (.. vscode -Range) 0 0 1 0)))]
+                (go
+                  (<p! (.. vscode -workspace (applyEdit edit1)))
+                  (<p! (.. vscode -workspace (applyEdit edit2))))))
+            visible-editors))))
+
+(defn register-event-handlers
+  [^js context]
+  (.. context -subscriptions
+      (push (.. vscode -workspace
+                (onDidChangeConfiguration handle-toggle-references-code-lens)))))
+
 (defn activate [^js context]
   (let [jar-path (. path join (. context -extensionPath) "clojure-lsp.jar")
         client (create-client jar-path)]
@@ -161,7 +191,8 @@
     (go
       (<p! (. client onReady))
       (.. state -cursor (set "lspClient" client))
-      (register-commands context client))))
+      (register-commands context client)
+      (register-event-handlers context))))
 
 (defn deactivate []
   (when-let [client (.. state deref (get "lspClient"))]
