a73fabcd043db84486e4555d12f5951419710b8c
Brandon Ringe <brandon.ringe@gmail.com>
2021-03-12 10:44:34 -0800

    Refactor download lsp code

--- src/cljs-lib/src/calva/lsp/download.cljs
+++ src/cljs-lib/src/calva/lsp/download.cljs
@@ -23,41 +23,42 @@
     (.. fs (readFileSync file-path "utf8"))))
 
 (defn unzip-file [zip-file-path extension-path]
+  (js/console.log "Unzipping file")
   (extract-zip zip-file-path
                (clj->js {:dir extension-path})))
 
-(defn download-clojure-lsp [extension-path version]
-  (js/console.log "Downloading clojure-lsp")
+(defn download-zip-file [url-path file-path]
+  (js/console.log "Downloading clojure-lsp from" url-path)
   (js/Promise.
    (fn [resolve reject]
-     (let [current-version (read-version-file extension-path)
-           url-path (str "/clojure-lsp/clojure-lsp/releases/download/"
-                         version
-                         "/" zip-file-name)
-           zip-file-path (get-zip-file-path extension-path)]
-       (js/console.log "Current version of clojure-lsp is" current-version)
-       (if (not= current-version version)
-         (do
-           (js/console.log "Downloading clojure-lsp from" url-path)
-           (.. https
-               (get (clj->js {:hostname "github.com"
-                              :path url-path})
-                    (fn [^js response]
-                      (js/console.log "Got response")
-                      (let [write-stream (.. fs (createWriteStream zip-file-path))]
-                        (.. response
-                            (on "end"
-                                (fn []
-                                  (.. write-stream close)
-                                  (js/console.log "Clojure-lsp zip file downloaded to" zip-file-path)
-                                  (.. (unzip-file zip-file-path extension-path)
-                                      (then
-                                       (fn []
-                                         (js/console.log "Zip file unzipped")
-                                         (resolve))))))
-                            (pipe write-stream)))))))
-         (do (js/console.log "Not downloading clojure-lsp")
-             (resolve)))))))
+     (.. https
+         (get (clj->js {:hostname "github.com"
+                        :path url-path})
+              (fn [^js response]
+                (js/console.log "Got response")
+                (let [write-stream (.. fs (createWriteStream file-path))]
+                  (.. response
+                      (on "end"
+                          (fn []
+                            (.. write-stream close)
+                            (js/console.log "Clojure-lsp zip file downloaded to" file-path)
+                            (resolve)))
+                      (pipe write-stream)))))))))
+
+(defn download-clojure-lsp [extension-path version]
+  (js/console.log "Downloading clojure-lsp")
+  (let [current-version (read-version-file extension-path)
+        url-path (str "/clojure-lsp/clojure-lsp/releases/download/"
+                      version
+                      "/" zip-file-name)
+        zip-file-path (get-zip-file-path extension-path)]
+    (js/console.log "Current version of clojure-lsp is" current-version)
+    (if (not= current-version version)
+      (.. (download-zip-file url-path zip-file-path)
+          (then (fn []
+                  (unzip-file zip-file-path extension-path))))
+      (do (js/console.log "Not downloading clojure-lsp")
+          (js/Promise.resolve)))))
 
 (comment
   (extract-zip "/home/brandon/development/calva/clojure-lsp-native-linux-amd64.zip"
