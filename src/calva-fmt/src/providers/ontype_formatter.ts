0018591cf4c959b697d11f2bf6ca486c0c201b69
Peter Strömberg <pez@pezius.com>
2021-04-04 18:16:25 +0200

    Make closing-bracket-prevention optional

--- src/calva-fmt/src/providers/ontype_formatter.ts
+++ src/calva-fmt/src/providers/ontype_formatter.ts
@@ -3,6 +3,7 @@ import * as formatter from '../format';
 import * as docMirror from '../../../doc-mirror/index';
 import { EditableDocument } from '../../../cursor-doc/model';
 import * as paredit from '../../../cursor-doc/paredit';
+import { getConfig } from '../../../config';
 
 
 function continueComment(editor: vscode.TextEditor, document: vscode.TextDocument, position: vscode.Position): Thenable<boolean> {
@@ -21,17 +22,20 @@ function continueComment(editor: vscode.TextEditor, document: vscode.TextDocumen
 
 export class FormatOnTypeEditProvider implements vscode.OnTypeFormattingEditProvider {
     async provideOnTypeFormattingEdits(document: vscode.TextDocument, _position: vscode.Position, ch: string, _options): Promise<vscode.TextEdit[]> {
+        const editor = vscode.window.activeTextEditor;
         let keyMap = vscode.workspace.getConfiguration().get('calva.paredit.defaultKeyMap');
         keyMap = String(keyMap).trim().toLowerCase();
-        if (keyMap === 'strict' && [')', ']', '}'].includes(ch)) {
-            const mDoc: EditableDocument = docMirror.getDocument(document);
-            return paredit.backspace(mDoc).then(fulfilled => {
-                console.log(fulfilled);
-                paredit.close(mDoc, ch);
+        if ([')', ']', '}'].includes(ch)) {
+            if (keyMap === 'strict' && getConfig().strictPreventUnmatchedClosingBracket) {
+                const mDoc: EditableDocument = docMirror.getDocument(document);
+                return paredit.backspace(mDoc).then(fulfilled => {
+                    paredit.close(mDoc, ch);
+                    return null;
+                });
+            } else {
                 return null;
-            });
+            }
         }
-        const editor = vscode.window.activeTextEditor;
 
         continueComment(editor, document, editor.selection.active).then(() => {
             const pos = editor.selection.active;
