eb68e2d56ac910642a958a63f5a6ade364a6c18a
Peter Strömberg <pez@pezius.com>
2021-04-20 10:32:04 +0200

    Smarten up the continue some

--- src/edit.ts
+++ src/edit.ts
@@ -1,5 +1,6 @@
 import * as vscode from 'vscode';
 import * as util from './utilities';
+import * as docMirror from './doc-mirror/index';
 
 export function continueCommentCommand() {
     const document = util.getDocument({});
@@ -7,21 +8,25 @@ export function continueCommentCommand() {
         const editor = vscode.window.activeTextEditor;
         const position = editor.selection.active;
         const eolPosition = position.with(position.line, Infinity);
-        const lineRange = new vscode.Range(position.with(position.line, 0), eolPosition);
-        const lineText: string = document.getText(lineRange),
-            match = lineText.match(/^[ \t]*;+[ \t]*/);
-        if (match) {
-            const [commentStart] = match;
-            //const edit = vscode.TextEdit.insert(eolPosition, `\n${commentStart}`);
-            //const wsEdit = new vscode.WorkspaceEdit();
-            //wsEdit.set(editor.document.uri, [edit]);
-            //vscode.workspace.applyEdit(wsEdit);
-            editor.edit(edits => edits.insert(eolPosition, `\n${commentStart}`), { undoStopAfter: false, undoStopBefore: true }).then(fulfilled => {
-                if (fulfilled) {
-                    const newEolPosition = eolPosition.with(eolPosition.line + 1, commentStart.length);
-                    editor.selection = new vscode.Selection(newEolPosition, newEolPosition);
-                }
-            });
+        const cursor = docMirror.getDocument(document).getTokenCursor();
+        if (!(cursor.getToken().type === 'comment')) {
+            if (cursor.getPrevToken().type === 'comment') {
+                cursor.previous();
+            } else {
+                cursor.next();
+            }
         }
+        const commentOffset = cursor.rowCol[1];
+        const commentText = cursor.getToken().raw;
+        const [_1, startText, bullet, num] =
+            commentText.match(/^([;\s]+)([*-] +|(\d+)\. )?/);
+        const pad = ' '.repeat(commentOffset);
+        const newText = `${pad}${startText}${num ? (parseInt(num) + 1) + '. ' : bullet ? bullet : ''}`;
+        editor.edit(edits => edits.insert(eolPosition, `\n${newText}`), { undoStopAfter: false, undoStopBefore: true }).then(fulfilled => {
+            if (fulfilled) {
+                const newEolPosition = eolPosition.with(eolPosition.line + 1, newText.length);
+                editor.selection = new vscode.Selection(newEolPosition, newEolPosition);
+            }
+        });
     }
 }
