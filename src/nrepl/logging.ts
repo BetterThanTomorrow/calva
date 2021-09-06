8fdd27b805356045a005d4f62bb7080fa30eb7a3
Brandon Ringe <brandon.ringe@gmail.com>
2021-09-05 17:50:18 -0700

    Refactor nrepl logging - WIP

--- src/nrepl/logging.ts
+++ src/nrepl/logging.ts
@@ -6,17 +6,20 @@ import * as nodeUtil from 'util';
 const NREPL_MESSAGES_CHANNEL_KEY = 'nReplMessagesChannel';
 const NREPL_MESSAGES_CHANNEL_NAME = 'nREPL Messages';
 
-if (getConfig().logNreplMessages === true) {
-    vscode.window.createOutputChannel(NREPL_MESSAGES_CHANNEL_NAME);
+function init(): void {
+    if (getConfig().logNreplMessages === true) {
+        const channel = vscode.window.createOutputChannel(NREPL_MESSAGES_CHANNEL_NAME);
+        setStateValue(NREPL_MESSAGES_CHANNEL_KEY, channel);
+    }
 }
 
-function formatNREPLMessage(message: any): string {
+function formatNreplMessage(message: any): string {
     return nodeUtil.inspect(message, false, 2, false);
 }
 
 function log(message: string): void {
     const nreplMessagesChannel: vscode.OutputChannel = getStateValue(NREPL_MESSAGES_CHANNEL_KEY);
     if (nreplMessagesChannel) {
-        nreplMessagesChannel.appendLine(formatNREPLMessage(message));
+        nreplMessagesChannel.appendLine(formatNreplMessage(message));
     }
 }
