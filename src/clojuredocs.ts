7fd9415122a05099ad93da16a78f8bf9b169b69e
Peter Strömberg <pez@pezius.com>
2021-10-24 14:40:58 +0200

    Add DocsEntry type

--- src/clojuredocs.ts
+++ src/clojuredocs.ts
@@ -8,6 +8,20 @@ import { getConfig } from './config';
 import * as replSession from './nrepl/repl-session';
 import * as getText from './util/get-text';
 
+export type DocsEntry = {
+    name: string;
+    type: string;
+    ns: string;
+    doc: string;
+    argsLists: string[];
+    baseUrl: string;
+    path: string;
+    examples: string[];
+    notes: string[];
+    seeAlsos: string[];
+    added: string;
+    tag: string;
+};
 
 export function init(cljSession: NReplSession) {
     cljSession.clojureDocsRefreshCache().catch(reason => {
@@ -15,21 +29,37 @@ export function init(cljSession: NReplSession) {
     });
 }
 
-export async function clojureDocsLookupSymbol(symbol: string) {
-    const doc = util.getDocument({});
-    const ns = namespace.getNamespace(doc);
-    //const session = replSession.getSession(util.getFileType(doc));
-    const session = replSession.getSession("clj");
-    const docs = await session.clojureDocsLookup(ns, symbol);
-    console.log(docs);
+export async function clojureDocsCiderNReplLookup(session: NReplSession, symbol: string, ns: string): Promise<DocsEntry> {
+    const ciderNReplDocs = await session.clojureDocsLookup(ns, symbol);
+    return ciderNRepl2DocsEntry(ciderNReplDocs.clojuredocs);
 }
 
 export async function clojureDocsLookup() {
     const doc = util.getDocument({});
     const position = vscode.window.activeTextEditor.selection.active;
     const symbol = util.getWordAtPosition(doc, position);
+    const ns = namespace.getNamespace(doc);
+    const session = replSession.getSession(util.getFileType(doc));
 
-    await clojureDocsLookupSymbol(symbol);
+    const docs = await clojureDocsCiderNReplLookup(session, symbol, ns);
+    console.log(docs);
+}
+
+function ciderNRepl2DocsEntry(docs: any): DocsEntry {
+    return {
+        name: docs.name,
+        added: docs.added,
+        baseUrl: 'https://clojuredocs.org/',
+        doc: docs.doc,
+        argsLists: docs.argsLists,
+        examples: docs.examples,
+        notes: docs.notes,
+        ns: docs.ns,
+        path: docs.href,
+        seeAlsos: docs['see-alsos'],
+        tag: docs.tag,
+        type: docs.type
+    }
 }
 
 //export function getHoverText() {
