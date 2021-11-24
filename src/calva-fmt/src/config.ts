1aaa047116a0400452da0d0efffe74b480f6252a
Peter Strömberg <pez@pezius.com>
2021-11-24 19:03:23 +0100

    Only invoke extra processChanges when Parinfer ON

--- src/calva-fmt/src/config.ts
+++ src/calva-fmt/src/config.ts
@@ -70,12 +70,14 @@ function _updateConfig(): FormatConfig {
     if (config['infer-parens-as-you-type']) {
         editorConfig.update("autoClosingBrackets", 'never', true, true);
         editorConfig.update("autoClosingOvertype", 'never', true, true);
+        editorConfig.update("formatOnPaste", false, true, true);
     } else {
         let keyMap = vscode.workspace.getConfiguration().get('calva.paredit.defaultKeyMap');
         keyMap = String(keyMap).trim().toLowerCase();
         const isStrict = keyMap === 'strict';
         editorConfig.update("autoClosingBrackets", isStrict ? 'always' : 'never', true, true);                
         editorConfig.update("autoClosingOvertype", isStrict ? 'always' : 'never', true, true);                
+        editorConfig.update("formatOnPaste", true, true, true);
     }
 
     maybeNagAboutParinferExtension(config);
