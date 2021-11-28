0eb8396e14b1124be051ca750a2c2c1fd41b6d67
Brandon Ringe <brandon.ringe@gmail.com>
2021-11-28 15:06:01 -0800

    Add resolveCodeAction middleware - WIP

--- src/lsp/main.ts
+++ src/lsp/main.ts
@@ -12,6 +12,7 @@ import * as path from 'path';
 import * as state from '../state';
 import { provideHover } from '../providers/hover';
 import { provideSignatureHelp } from '../providers/signature';
+import { ProviderResult, CodeAction } from 'vscode';
 
 const LSP_CLIENT_KEY = 'lspClient';
 const RESOLVE_MACRO_AS_COMMAND = 'resolve-macro-as';
@@ -40,6 +41,11 @@ function createClient(clojureLspPath: string): LanguageClient {
         middleware: {
             provideLinkedEditingRange: async (_document, _position, _token, _next): Promise<vscode.LinkedEditingRanges> => {
                 return null;
+            },
+            resolveCodeAction(item, token, next): ProviderResult<CodeAction> {
+                console.log(item);
+                // TODO: See if next returns codeAction/resolve response and then use info from that to call lsp command
+                return next(item, token);
             },
             handleDiagnostics(uri, diagnostics, next) {
                 if (uri.path.endsWith(config.REPL_FILE_EXT)) {
