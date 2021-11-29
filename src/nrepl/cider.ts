9ba326ec07fd2621d672ee095e0b661009fe7f42
Marc O'Morain <marc@circleci.com>
2021-11-29 15:36:54 +0000

    Extract error message handling into functions
    I want to re-use some of this infrastructure when adding Test Explorer
    support, so I'd like to extract this code out into smaller, composable
    functions. This will allow me to re-use them (and test them).
    
    Changes:
    
    - Extract the code for adding summaries together into `totalSummary`.
    - Extract the summary message generation into `summaryMessage.
    - Replace uses of _ with native TypeScript looping constructs.

--- src/nrepl/cider.ts
+++ src/nrepl/cider.ts
@@ -37,3 +37,51 @@ export interface TestResults {
     'testing-ns'?: string
     'gen-input': unknown
 }
+
+
+export function resultMessage(resultItem: Readonly<TestResult>): string {
+    let msg = [];
+    if (resultItem.context && resultItem.context !== "false")
+        msg.push(resultItem.context);
+    if (resultItem.message)
+        msg.push(resultItem.message);
+    return `${msg.length > 0 ? msg.join(": ").replace(/\r?\n$/, "") : ''}`;
+}
+
+// Given a summary, return a message suitable for printing in the REPL to show
+// the user a quick summary of the test run.
+// Examples:
+// ; No tests found. 😱, ns: 0, vars: 0
+// ; 6 tests finished, all passing 👍, ns: 1, vars: 2
+// ; 6 tests finished, problems found. 😭 errors: 0, failures: 1, ns: 1, vars: 2
+export function summaryMessage(summary: Readonly<TestSummary>): string {
+    let msg = [];
+    if (summary.test > 0) {
+        msg.push(summary.test + " tests finished");
+
+        const hasProblems = summary.error + summary.fail > 0;
+        if (!hasProblems) {
+            msg.push("all passing 👍");
+        } else {
+            msg.push("problems found. 😭 errors: " + summary.error + ", failures: " + summary.fail);
+        }
+
+    } else {
+        msg.push("No tests found. 😱");
+    }
+
+    msg.push("ns: " + summary.ns + ", vars: " + summary.var);
+    return msg.join(", ");
+}
+
+// Given a list of summaries, sum them to compute the total number of tests,
+// errors and vars, etc.
+export function totalSummary(summaries: TestSummary[]): TestSummary {
+    let result = { test: 0, error: 0, ns: 0, var: 0, fail: 0, pass: 0 };
+    for (let summary of summaries) {
+        for (const k in result) {
+            result[k] = result[k] + summary[k];
+        }
+    }
+    return result;
+}
