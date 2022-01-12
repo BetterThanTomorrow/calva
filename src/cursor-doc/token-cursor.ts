96bc9500846f88b59eec1aafd5f55d96e7b43608
Steve Ashton <nothsaevets@gmail.com>
2022-01-12 16:25:42 -0500

    Revert "Fix forwardSexp function for metadata skipping"
    This reverts commit a6dcca117b08b92c996bb9c3847e75ac9d1caf9a.

--- src/cursor-doc/token-cursor.ts
+++ src/cursor-doc/token-cursor.ts
@@ -229,7 +229,6 @@ export class LispTokenCursor extends TokenCursor {
         if (this.getToken().type === 'close') {
             return false;
         }
-        const initialToken = this.getToken();
         if (this.tokenBeginsMetadata()) {
             isMetadata = true;
         }
@@ -259,15 +258,7 @@ export class LispTokenCursor extends TokenCursor {
                     if (skipMetadata && this.getToken().raw.startsWith('^')) {
                         this.next();
                     } else {
-                        this.forwardWhitespace(skipComments);
-                        // If the cursor is still pointed at the original token,
-                        // or if the cursor is inside a nested sexp, move the cursor forward by one.
-                        // Otherwise, the cursor has already moved past
-                        // the original token, and should not be moved again 
-                        // for this invocation of forwardSexp.
-                        if (token == initialToken || stack.length > 0) {
-                          this.next();
-                        }
+                        this.next();
                         if (stack.length <= 0) {
                             return true;
                         }
