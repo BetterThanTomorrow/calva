93b9416a0f821d36102948ddbb1f2cb0e8f1ff00
Cora Sutton <cora@sutton.me>
2022-02-10 20:36:01 -0600

    Add typescript-eslint packages

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+    root: true,
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint'],
+    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+    env: {
+        browser: true,
+        commonjs: true,
+        es6: true,
+        node: true,
+    },
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        sourceType: 'module',
+    },
+    rules: {
+        'no-const-assign': 'warn',
+        'no-this-before-super': 'warn',
+        'no-undef': 'warn',
+        'no-unreachable': 'warn',
+        'no-unused-vars': 'warn',
+        'constructor-super': 'warn',
+        'valid-typeof': 'warn',
+    },
+};
