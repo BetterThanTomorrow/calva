508ffea4c07e75f95f101e1f4c834a4f9926204d
Lukas Domagala <domagala.lukas@gmail.com>
2022-03-06 20:26:57 +0100

    fixed snipped and added it to docs

--- test-data/integration-test/test.clj
+++ test-data/integration-test/test.clj
@@ -3,7 +3,7 @@
 (def hover-map
   {:stuff "in-here"
    :ohter "yeah"
-   :deeper {:a 1, "foo" :bar, [1 2 3] (range 1)}})
+   :deeper {:a 1, "foo" :bar, [1 2 3] (range 100)}})
 
 (defn foo []
   (println "bar"))
