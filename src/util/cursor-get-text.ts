5cbe32c23ccba3e6e34a5f9863c45f6dc7281e09
Lukas Domagala <domagala.lukas@gmail.com>
2022-03-07 20:01:45 +0100

    added default to currentTopLevelFunction

--- src/util/cursor-get-text.ts
+++ src/util/cursor-get-text.ts
@@ -8,7 +8,7 @@ export type RangeAndText = [[number, number], string];
 
 export function currentTopLevelFunction(
     doc: EditableDocument,
-    active: number
+    active: number = doc.selection.active
 ): RangeAndText {
     const defunCursor = doc.getTokenCursor(active);
     const defunStart = defunCursor.rangeForDefun(active)[0];
