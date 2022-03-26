78620f31cde56aa9d1812c27c7d019e84973b77f
Peter Strömberg <pez@pezius.com>
2022-03-26 17:18:19 +0100

    Add integration test for copy jack-in cmd

--- src/extension-test/integration/suite/util.ts
+++ src/extension-test/integration/suite/util.ts
@@ -1,4 +1,5 @@
 import * as path from 'path';
+import * as vscode from 'vscode';
 
 export const testDataDir = path.join(
   __dirname,
@@ -6,3 +7,23 @@ export const testDataDir = path.join(
   'test-data',
   'integration-test'
 );
+
+export async function openFile(filePath: string) {
+  const uri = vscode.Uri.file(filePath);
+  const document = await vscode.workspace.openTextDocument(uri);
+  const editor = await vscode.window.showTextDocument(document);
+
+  await sleep(300);
+
+  return editor;
+}
+
+export function sleep(ms: number): Promise<void> {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
+
+export function log(suite: string, ...things: any[]) {
+  console.log(`Integration testing, ${suite}:`, ...things);
+}
