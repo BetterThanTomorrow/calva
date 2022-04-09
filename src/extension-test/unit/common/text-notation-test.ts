0d372202892f021816b1e87b8ae9fd0ace1637da
Peter Strömberg <pez@pezius.com>
2022-04-09 17:44:44 +0200

    Add failing test for docFromTextNotation

--- src/extension-test/unit/common/text-notation-test.ts
+++ src/extension-test/unit/common/text-notation-test.ts
@@ -1,13 +1,23 @@
 import * as expect from 'expect';
-import { docFromTextNotation, textNotationFromDoc } from '../common/text-notation';
-import _ = require('lodash');
+import * as textNotation from '../common/text-notation';
 
 describe('text-notation test utils', () => {
   describe('textNotationFromDoc', () => {
     it('should return the same input text to textNotationFromDoc', () => {
       const inputText = '(a b|1) (a b|) (a (b))';
-      const doc = docFromTextNotation(inputText);
-      expect(textNotationFromDoc(doc)).toEqual(inputText);
+      const doc = textNotation.docFromTextNotation(inputText);
+      expect(textNotation.textNotationFromDoc(doc)).toEqual(inputText);
+    });
+  });
+  describe('docFromTextNotation', () => {
+    it('creates single cursor position', () => {
+      const tn = '(a b|1)';
+      const text = '(a b)';
+      // TODO: Figure out why we need undefined here?
+      // const selections = [undefined, [4, 4]];
+      const selections = [[4, 4]];
+      const doc = textNotation.docFromTextNotation(tn);
+      expect(textNotation.textAndSelection(doc)).toEqual([text, selections]);
     });
   });
 });
