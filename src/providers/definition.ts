e8c4cb17e9e016b30ab8d23cce4a30c27f60e08c
Cora Sutton <cora@sutton.me>
2022-05-01 02:02:40 -0500

    Fix strict null check issues for src/providers/definition.ts

--- src/providers/definition.ts
+++ src/providers/definition.ts
@@ -21,6 +21,7 @@ async function provideClojureDefinition(document, position: vscode.Position, _to
   if (util.getConnectedState() && !posIsEvalPos) {
     const text = util.getWordAtPosition(document, position);
     const client = replSession.getSession(util.getFileType(document));
+    util.assertIsDefined(client, 'Expected there to be a repl client!');
     const info = await client.info(namespace.getNamespace(document), text);
     if (info.file && info.file.length > 0) {
       const pos = new vscode.Position(info.line - 1, info.column || 0);
