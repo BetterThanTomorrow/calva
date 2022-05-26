140ae4812ccc4f8f1be278ba1baed6da1a7080f9
Peter Strömberg <pez@pezius.com>
2022-05-26 18:21:17 +0200

    Change to `v0`

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,7 +2,10 @@ import * as evaluate from './evaluate-code';
 
 export function getApi() {
   return {
-    v1: {
+    // If we can avoid it we don't want to ever break our callers.
+    // This first version of the API is a draft though, so signaling
+    // potential removal of this version of the API with the `0`.
+    v0: {
       evaluateCode: evaluate.evaluateCode,
     },
   };
