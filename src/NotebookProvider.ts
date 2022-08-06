9901a0d80bad987f9eb30cbd34b466e5f2cf21fd
Lukas Domagala <domagala.lukas@gmail.com>
2022-08-06 23:44:15 +0200

    notebook support

---
+++ src/NotebookProvider.ts
@@ -0,0 +1,120 @@
+import * as vscode from 'vscode';
+import { TextDecoder, TextEncoder } from 'util';
+import { parseForms, prettyPrint } from '../out/cljs-lib/cljs-lib';
+import { LispTokenCursor } from './cursor-doc/token-cursor';
+import { LineInputModel } from './cursor-doc/model';
+import * as repl from './api/repl';
+
+export interface RawNotebookCell {
+  language: string;
+  content: string;
+  kind: vscode.NotebookCellKind;
+}
+
+export class NotebookProvider implements vscode.NotebookSerializer {
+  private readonly decoder = new TextDecoder();
+  private readonly encoder = new TextEncoder();
+
+  deserializeNotebook(
+    data: Uint8Array,
+    _token: vscode.CancellationToken
+  ): vscode.NotebookData | Thenable<vscode.NotebookData> {
+    const content = this.decoder.decode(data);
+    console.log(content);
+    const cellRawData = parseClojure(content);
+
+    return {
+      cells: cellRawData,
+    };
+  }
+
+  serializeNotebook(
+    data: vscode.NotebookData,
+    _token: vscode.CancellationToken
+  ): Uint8Array | Thenable<Uint8Array> {
+    const stringOutput = writeCellsToClojure(data.cells);
+    return this.encoder.encode('stringOutput');
+  }
+}
+
+function parseClojure(content: string): vscode.NotebookCellData[] {
+  const model = new LineInputModel();
+  model.insertString(0, content);
+  const cursor = model.getTokenCursor(0);
+  const ranges = cursor.rangesForTopLevelForms().map(([start, end]) => {
+    return {
+      value: content.substring(start, end),
+      kind: vscode.NotebookCellKind.Code,
+      languageId: 'clojure',
+    };
+  });
+  console.log(ranges);
+  return ranges;
+}
+
+function writeCellsToClojure(cells: vscode.NotebookCellData[]) {
+  throw new Error('Function not implemented.');
+}
+
+export class NotebookKernel {
+  readonly id: string = 'calva-book-kernel';
+  readonly notebookType: string = 'calva-notebook';
+  readonly label: string = 'Calva Notebook';
+  readonly supportedLanguages = ['clojure'];
+
+  private readonly _controller: vscode.NotebookController;
+
+  constructor() {
+    this._controller = vscode.notebooks.createNotebookController(
+      this.id,
+      this.notebookType,
+      this.label,
+      executeAll
+    );
+
+    this._controller.supportedLanguages = this.supportedLanguages;
+  }
+
+  dispose(): void {
+    this._controller.dispose();
+  }
+}
+
+async function executeAll(
+  cells: vscode.NotebookCell[],
+  _notebook: vscode.NotebookDocument,
+  controller: vscode.NotebookController
+) {
+  for (const cell of cells) {
+    await doExecution(cell, controller);
+  }
+}
+
+async function doExecution(
+  cell: vscode.NotebookCell,
+  controller: vscode.NotebookController
+): Promise<void> {
+  const execution = controller.createNotebookCellExecution(cell);
+  execution.start(Date.now());
+
+  try {
+    const response = repl.evaluateCode(undefined, cell.document.getText());
+
+    await execution.replaceOutput([
+      new vscode.NotebookCellOutput([
+        vscode.NotebookCellOutputItem.text(prettyPrint((await response).result).value),
+      ]),
+    ]);
+    execution.end(true, Date.now());
+  } catch (err) {
+    await execution.replaceOutput([
+      new vscode.NotebookCellOutput([
+        vscode.NotebookCellOutputItem.error({
+          name: (err instanceof Error && err.name) || 'error',
+          message: (err instanceof Error && err.message) || JSON.stringify(err, undefined, 4),
+        }),
+      ]),
+    ]);
+    execution.end(false, Date.now());
+  }
+}
