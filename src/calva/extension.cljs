25887c8c3cfe13b366476f3d895b163f921141c6
Brandon Ringe <12722744+bpringe@users.noreply.github.com>
2023-01-15 15:48:22 -0800

    Add "extension" build for shadow

---
+++ src/calva/extension.cljs
@@ -0,0 +1,44 @@
+(ns calva.extension
+  (:require ["vscode" :as vscode :refer [window]]))
+
+(defonce current-context (atom nil))
+(defonce disposables (atom []))
+
+(defn add-disposable! [disposable]
+  (swap! disposables conj disposable))
+
+(defn dispose-all!
+  [disposables]
+  (run! (fn [^js disposable]
+          (.. disposable (dispose)))
+        disposables))
+
+(defn say-hello []
+  (.. window (showInformationMessage "Hello world!")))
+
+(defn register-command!
+  [command-name command-function]
+  (let [disposable (.. vscode -commands (registerCommand
+                                         command-name
+                                         command-function))]
+    (add-disposable! disposable)))
+
+(defn activate
+  [^js context]
+  (reset! current-context context)
+  (register-command! "calva.sayHello" say-hello)
+  (prn "Calva activated"))
+
+(defn deactivate
+  []
+  (dispose-all! @disposables)
+  (prn "Calva deactivated"))
+
+(defn before-load-async [done]
+  (deactivate)
+  (done))
+
+(defn after-load []
+  (activate @current-context)
+  (prn "Calva reloaded"))
+
