950a8e5bfa0b17847ac637c2c96bd1326c8a1cdc
Brandon Ringe <12722744+bpringe@users.noreply.github.com>
2023-01-21 19:45:44 -0800

    Change output location of cljs-lib back to out dir

--- src/foo.ts
+++ src/foo.ts
@@ -1,7 +1,18 @@
+//import * as vscode from 'vscode';
+import * as os from 'os';
+
 const bar = 'bar';
 
 function hello() {
   return 'hello';
 }
+
+function platform() {
+  return os.platform();
+}
+
+// function showMessage(message) {
+//   void vscode.window.showInformationMessage(message);
+// }
 
 export { bar, hello };
