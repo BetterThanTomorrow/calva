6968f7bc8d499b714a254ea79fde89c16cfff1dc
Peter Strömberg <pez@pezius.com>
2023-02-05 10:21:29 +0100

    Grap cursor position from notation

--- src/extension-test/unit/formatting-test.ts
+++ src/extension-test/unit/formatting-test.ts
@@ -23,9 +23,9 @@ describe('formatter, indenter and paredit comparison', () => {
 
   configs.forEach((config) => {
     it(`correctly formats 'and' form with formatter, indenter and paredit using config: ${config['cljfmt-options-string']}`, () => {
-      const formatterIndent = getFormatterIndent('(and x\ny)', 7, config);
+      const formatterIndent = getFormatterIndent('(and x\n|y)', config);
       const indenterIndent = getIndenterIndent('(and x\n|y)', config);
-      const pareditIndent = getPareditIndent('(and x\n\n  |y)', 10, config);
+      const pareditIndent = getPareditIndent('(and x\n\n  |y)', config);
 
       expect(formatterIndent).toEqual(indenterIndent);
       expect(formatterIndent).toEqual(pareditIndent);
@@ -34,18 +34,23 @@ describe('formatter, indenter and paredit comparison', () => {
 });
 
 function getIndenterIndent(form: string, config: ReturnType<typeof mkConfig>) {
-  const indenterDoc = docFromTextNotation(form);
-  return indent.getIndent(indenterDoc.model, textAndSelection(indenterDoc)[1][0], config);
+  const doc = docFromTextNotation(form);
+  const p = textAndSelection(doc)[1][0];
+  return indent.getIndent(doc.model, p, config);
 }
 
-function getFormatterIndent(form: string, cursor: number, config: ReturnType<typeof mkConfig>) {
-  const formatterResult = formatIndex(form, [0, form.length], cursor, '\n', false, config);
+function getFormatterIndent(notation: string, config: ReturnType<typeof mkConfig>) {
+  const doc = docFromTextNotation(notation);
+  const form = textAndSelection(doc)[0];
+  const p = textAndSelection(doc)[1][0];
+  const formatterResult = formatIndex(form, [0, notation.length], p, '\n', false, config);
   return formatterResult['new-index'] - formatterResult.idx;
 }
 
-function getPareditIndent(form: string, cursor: number, config: ReturnType<typeof mkConfig>) {
-  const pareditDoc = docFromTextNotation(form);
-  return backspaceOnWhitespace(pareditDoc, pareditDoc.getTokenCursor(cursor), config).indent;
+function getPareditIndent(notation: string, config: ReturnType<typeof mkConfig>) {
+  const doc = docFromTextNotation(notation);
+  const p = textAndSelection(doc)[1][0];
+  return backspaceOnWhitespace(doc, doc.getTokenCursor(p), config).indent;
 }
 
 function mkConfig(rules: indent.IndentRules, rulesString: string) {
