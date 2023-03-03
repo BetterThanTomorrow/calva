da9ea55dfd7e6acbfd86c37492b97b20e2af5c8f
Brandon Ringe <12722744+bpringe@users.noreply.github.com>
2023-03-03 14:45:30 -0800

    Append to markdown file

--- src/repl-output.ts
+++ src/repl-output.ts
@@ -1,4 +1,5 @@
 import * as vscode from 'vscode';
+import * as fs from 'fs/promises';
 
 export async function init(context: vscode.ExtensionContext) {
   // Create the repl output markdown file
@@ -6,8 +7,13 @@ export async function init(context: vscode.ExtensionContext) {
   const wsedit = new vscode.WorkspaceEdit();
   const wsPath = vscode.workspace.workspaceFolders[0].uri.fsPath; // gets the path of the first workspace folder
   const filePath = vscode.Uri.file(wsPath + '/' + fileName);
-  void vscode.window.showInformationMessage(filePath.toString());
   wsedit.createFile(filePath, { ignoreIfExists: true });
   await vscode.workspace.applyEdit(wsedit);
-  void vscode.window.showInformationMessage('Created a new file: repl-output.md');
+  //void vscode.window.showInformationMessage('Created a new file: ' + fileName);
+
+  await vscode.commands.executeCommand('markdown.showPreviewToSide', filePath);
+
+  setInterval(async () => {
+    await fs.appendFile(filePath.fsPath, '\n```clojure\n(+ 1 2 3 4 5)\n```');
+  }, 500);
 }
