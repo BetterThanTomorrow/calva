3266703be4237d45925b268dc7966775f7ddeb35
Peter Strömberg <pez@pezius.com>
2023-06-24 13:12:16 +0200

    Make StringDocument more robust against unbalance

--- src/extension-test/unit/cursor-doc/utilities-test.ts
+++ src/extension-test/unit/cursor-doc/utilities-test.ts
@@ -9,6 +9,11 @@ describe('Utilities that need cursor doc things', () => {
       const trail = ']})';
       expect(utilities.addMissingBrackets(text)).toEqual(`${text}${trail}`);
     });
+    it('Closes strings', () => {
+      const text = '(a b) (c {:d [1 2 3 "four';
+      const trail = '"]})';
+      expect(utilities.addMissingBrackets(text)).toEqual(`${text}${trail}`);
+    });
     it('Leaves non-broken structure alone', () => {
       const text = '(a b) (c {:d [1 2 3 "four"] :e :f} g) h';
       expect(utilities.addMissingBrackets(text)).toEqual(text);
@@ -26,12 +31,6 @@ describe('Utilities that need cursor doc things', () => {
     it('Ignores brackets in line comments', () => {
       const text = '(a b) (c {:d \n; ( [ ( {\n[1 2 3 "four"';
       const trail = ']})';
-      expect(utilities.addMissingBrackets(text)).toEqual(`${text}${trail}`);
-    });
-    xit('Closes strings', () => {
-      // TODO: Fix the scanner or whatever it is that makes this crash everything else
-      const text = '(a b) (c {:d [1 2 3 "four';
-      const trail = '"]})';
       expect(utilities.addMissingBrackets(text)).toEqual(`${text}${trail}`);
     });
   });
