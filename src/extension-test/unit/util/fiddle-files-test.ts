5afd282ca38d46ceb668e6d5ac8defd51bb8972c
Peter Strömberg <pez@pezius.com>
2023-07-02 00:48:24 +0200

    Add getSourceForFiddleFile function

--- src/extension-test/unit/util/fiddle-files-test.ts
+++ src/extension-test/unit/util/fiddle-files-test.ts
@@ -3,16 +3,127 @@ import * as fs from 'fs';
 import * as fiddleFiles from '../../../util/fiddle-files';
 
 describe('fiddle files', () => {
-  it('without source->fiddle map, gets fiddle file for cljc file', function () {
-    expect(fiddleFiles.getFiddleForFilePath('/u/p/src/a/b/c-d.cljc', '/u/p', null)).toBe(
-      '/u/p/src/a/b/c-d.fiddle'
-    );
+  describe('fiddle file for source', () => {
+    it('without source->fiddle map, gets fiddle file for cljc file', function () {
+      expect(fiddleFiles.getFiddleForSourceFile('/u/p/src/a/b/c-d.cljc', '/u/p', null)).toBe(
+        '/u/p/src/a/b/c-d.fiddle'
+      );
+    });
+    it('with source->fiddle map, gets fiddle file for cljc file', function () {
+      expect(
+        fiddleFiles.getFiddleForSourceFile('/u/p/src/a/b/c-d.cljc', '/u/p', [
+          { source: ['src'], fiddle: ['dev'] },
+        ])
+      ).toBe('/u/p/dev/a/b/c-d.cljc');
+    });
+    it('with source->fiddle map with several matching source mappings, gets fiddle file for first', function () {
+      expect(
+        fiddleFiles.getFiddleForSourceFile('/u/p/src/a/b/c-d.cljc', '/u/p', [
+          { source: ['no-match'], fiddle: ['no-fiddle'] },
+          { source: ['src'], fiddle: ['first'] },
+          { source: ['src'], fiddle: ['second'] },
+        ])
+      ).toBe('/u/p/first/a/b/c-d.cljc');
+    });
+    it('throws when no source mapping', function () {
+      expect(() =>
+        fiddleFiles.getFiddleForSourceFile('/u/p/src/a/b/c-d.cljc', '/u/p', [
+          { source: ['no-match'], fiddle: ['no-fiddle'] },
+        ])
+      ).toThrow();
+    });
+    it('throws when project root does not match', function () {
+      expect(() =>
+        fiddleFiles.getFiddleForSourceFile('/u/p/src/a/b/c-d.cljc', '/u/p-no', [
+          { source: ['no-match'], fiddle: ['no-fiddle'] },
+          { source: ['src'], fiddle: ['first'] },
+          { source: ['src'], fiddle: ['second'] },
+        ])
+      ).toThrow();
+    });
   });
-  it('with source->fiddle map, gets fiddle file for cljc file', function () {
-    expect(
-      fiddleFiles.getFiddleForFilePath('/u/p/src/a/b/c-d.cljc', '/u/p', [
-        { source: ['src'], fiddle: ['dev'] },
-      ])
-    ).toBe('/u/p/dev/a/b/c-d.cljc');
+
+  describe('source for fiddle file', () => {
+    describe('source base', () => {
+      it('without fiddle->source map, gets source base for fiddle file', function () {
+        expect(
+          fiddleFiles.getSourceBaseForFiddleFile('/u/p/src/a/b/c-d.fiddle', '/u/p', null)
+        ).toBe('/u/p/src/a/b/c-d');
+      });
+      it('with fiddle->source map, gets source base for fiddle file', function () {
+        expect(
+          fiddleFiles.getSourceBaseForFiddleFile('/u/p/dev/a/b/c-d.clj', '/u/p', [
+            { source: ['src'], fiddle: ['dev'] },
+          ])
+        ).toBe('/u/p/src/a/b/c-d');
+      });
+      it('with fiddle->source map with several matching source mappings, gets fiddle file for first', function () {
+        expect(
+          fiddleFiles.getSourceBaseForFiddleFile('/u/p/dev/a/b/c-d.cljc', '/u/p', [
+            { source: ['no-source'], fiddle: ['no-match'] },
+            { source: ['first'], fiddle: ['dev'] },
+            { source: ['second'], fiddle: ['dev'] },
+          ])
+        ).toBe('/u/p/first/a/b/c-d');
+      });
+      it('throws when no fiddle mapping', function () {
+        expect(() =>
+          fiddleFiles.getSourceBaseForFiddleFile('/u/p/dev/a/b/c-d.fiddle', '/u/p', [
+            { source: ['no-source'], fiddle: ['no-match'] },
+          ])
+        ).toThrow();
+      });
+      it('throws when project root does not match', function () {
+        expect(() =>
+          fiddleFiles.getSourceBaseForFiddleFile('/u/p/dev/a/b/c-d.cljc', '/u/p-no', [
+            { source: ['no-source'], fiddle: ['no-match'] },
+            { source: ['first'], fiddle: ['dev'] },
+            { source: ['second'], fiddle: ['dev'] },
+          ])
+        ).toThrow();
+      });
+    });
+
+    describe('source file', () => {
+      const mockWorkspace: fiddleFiles.Workspace = {
+        findFiles: (pattern: string) => {
+          return Promise.resolve([
+            { fsPath: '/u/p/src/a/b/c-d.clj' },
+            { fsPath: '/u/p/src/a/b/c-d.cljc' },
+          ]);
+        },
+      };
+      it('without fiddle->source map, gets cljc source file for fiddle file', async function () {
+        expect(
+          await fiddleFiles.getSourceForFiddleFile(
+            '/u/p/src/a/b/c-d.fiddle',
+            '/u/p',
+            null,
+            mockWorkspace,
+            ['cljc', 'clj']
+          )
+        ).toBe('/u/p/src/a/b/c-d.cljc');
+      });
+      it('with fiddle->source map, gets source clj for clj fiddle file', async function () {
+        expect(
+          await fiddleFiles.getSourceForFiddleFile(
+            '/u/p/dev/a/b/c-d.clj',
+            '/u/p',
+            [{ source: ['src'], fiddle: ['dev'] }],
+            mockWorkspace
+          )
+        ).toBe('/u/p/src/a/b/c-d.clj');
+      });
+      it('with fiddle->source map, gets source bb for bb fiddle file', async function () {
+        expect(
+          await fiddleFiles.getSourceForFiddleFile(
+            '/u/p/dev/a/b/c-d.bb',
+            '/u/p',
+            [{ source: ['src'], fiddle: ['dev'] }],
+            mockWorkspace
+          )
+        ).toBe('/u/p/src/a/b/c-d.bb');
+      });
+    });
   });
 });
--- src/util/fiddle-files.ts
+++ src/util/fiddle-files.ts
@@ -1,5 +1,4 @@
 import * as path from 'path';
-import * as fs from 'fs';
 
 export type SourceToFiddleFilePaths = Array<{
   source: string[];
@@ -9,18 +8,18 @@ export type SourceToFiddleFilePaths = Array<{
 export const FIDDLE_FILE_EXTENSION = 'fiddle';
 
 const clojureFileExtensions = [
+  'cljc',
   'clj',
   'cljs',
-  'cljc',
   'cljd',
   'cljr',
+  'bb',
   'cljx',
   'clojure',
   'joke',
-  'bb',
 ];
 
-export function getFiddleForFilePath(
+export function getFiddleForSourceFile(
   filePath: string,
   projectRootPath: string,
   sourceToFiddleFilePaths: SourceToFiddleFilePaths
@@ -31,7 +30,7 @@ export function getFiddleForFilePath(
       filePath.length - path.extname(filePath).length
     )}.${FIDDLE_FILE_EXTENSION}`;
   }
-  
+
   const sourceMappings = sourceToFiddleFilePaths.filter((mapping) => {
     const sourcePath = path.join(projectRootPath, ...mapping.source);
     return filePath.startsWith(sourcePath);
@@ -43,4 +42,65 @@ export function getFiddleForFilePath(
   const sourcePath = path.join(projectRootPath, ...sourceMapping.source);
   const relativeFilePath = path.relative(sourcePath, filePath);
   return path.join(projectRootPath, ...sourceMapping.fiddle, relativeFilePath);
+}
+
+export function getSourceBaseForFiddleFile(
+  filePath: string,
+  projectRootPath: string,
+  sourceToFiddleFilePaths: SourceToFiddleFilePaths
+): string {
+  if (sourceToFiddleFilePaths === null) {
+    if (path.extname(filePath) !== `.${FIDDLE_FILE_EXTENSION}`) {
+      throw new Error(
+        `Expected fiddle file extension to be ${FIDDLE_FILE_EXTENSION}, but was ${path.extname(
+          filePath
+        )}`
+      );
+    }
+    return filePath.substring(0, filePath.length - path.extname(filePath).length);
+  }
+
+  const fiddleMappings = sourceToFiddleFilePaths.filter((mapping) => {
+    const fiddlePath = path.join(projectRootPath, ...mapping.fiddle);
+    return filePath.startsWith(fiddlePath);
+  });
+  if (fiddleMappings.length === 0) {
+    throw new Error(`No fiddle->source mapping found for file ${filePath}`);
+  }
+  const fiddleMapping = fiddleMappings[0];
+  const fiddlePath = path.join(projectRootPath, ...fiddleMapping.fiddle);
+  const relativeFilePath = path.relative(fiddlePath, filePath);
+  const relativeBasePath = relativeFilePath.substring(
+    0,
+    relativeFilePath.length - path.extname(relativeFilePath).length
+  );
+  return path.join(projectRootPath, ...fiddleMapping.source, relativeBasePath);
+}
+
+interface Uri {
+  fsPath: string;
+}
+
+export interface Workspace {
+  findFiles: (pattern: string) => Promise<Uri[]>;
+}
+
+export async function getSourceForFiddleFile(
+  filePath: string,
+  projectRootPath: string,
+  sourceToFiddleFilePaths: SourceToFiddleFilePaths,
+  workspace: Workspace,
+  extensions: string[] = clojureFileExtensions
+): Promise<string> {
+  const sourceBase = getSourceBaseForFiddleFile(filePath, projectRootPath, sourceToFiddleFilePaths);
+  console.log('sourceBase', sourceBase);
+  if (sourceToFiddleFilePaths === null) {
+    const uris = await workspace.findFiles(`${sourceBase}.{${extensions.join(',')}}`);
+    return uris.sort((a: Uri, b: Uri) => {
+      const extA = a.fsPath.split('.').pop() || '';
+      const extB = b.fsPath.split('.').pop() || '';
+      return extensions.indexOf(extA) - extensions.indexOf(extB);
+    })[0].fsPath;
+  }
+  return `${sourceBase}${path.extname(filePath)}`;
 }
