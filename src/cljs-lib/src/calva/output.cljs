0685ebcee80c300a3a6238feafb5cf75ddc60860
Brandon Ringe <12722744+bpringe@users.noreply.github.com>
2023-08-19 18:06:37 -0700

    Add ^js metadata

--- src/cljs-lib/src/calva/output.cljs
+++ src/cljs-lib/src/calva/output.cljs
@@ -1,10 +1,10 @@
 (ns calva.output)
 
-(defonce ^js vscode (atom nil))
+(defonce vscode (atom nil))
 
 (defn activate-output [^js vsc]
   (reset! vscode vsc)
-  (.. @vscode -window (showInformationMessage "hello output world")))
+  (.. ^js @vscode -window (showInformationMessage "hello output world")))
 
 (comment
   (.. @vscode -window (showInformationMessage "hello world"))
