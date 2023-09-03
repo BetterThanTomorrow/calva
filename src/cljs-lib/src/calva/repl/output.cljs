514f9df2ab44ba242028c77d8b91769bb6811f90
Brandon Ringe <12722744+bpringe@users.noreply.github.com>
2023-09-03 15:07:05 -0700

    Add dev code

--- src/cljs-lib/src/calva/repl/output.cljs
+++ src/cljs-lib/src/calva/repl/output.cljs
@@ -1,7 +1,18 @@
-(ns calva.repl.output)
+(ns calva.repl.output
+  (:require ["fs" :as fs]))
 
 (defonce vscode (atom nil))
 
 (defn activate [^js vsc]
   (reset! vscode vsc)
   (.. ^js @vscode -window (showInformationMessage "hello output world")))
+
+(defn create-repl-output-file [])
+
+(comment
+  (run!
+   #(.. fs (appendFileSync
+            "/Users/brandon/development/crescent-api/.calva/output-window/repl-output.md"
+            "\nhello world"))
+   (range 1000))
+  :rcf)
