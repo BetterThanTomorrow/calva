5871dfa09c0376d44b1edd35b98e6670a6357f7a
Brandon Ringe <12722744+bpringe@users.noreply.github.com>
2023-09-03 15:33:34 -0700

    Add cljs code to get project root uri

--- src/cljs-lib/src/calva/repl/output.cljs
+++ src/cljs-lib/src/calva/repl/output.cljs
@@ -1,7 +1,25 @@
 (ns calva.repl.output
-  (:require ["fs" :as fs]))
+  (:require ["fs" :as fs]
+            [calva.state :as state]))
 
 (defonce vscode (atom nil))
+
+(def project-root-uri-key "connect.projectDirNew")
+
+(defn get-first-workspace-folder-uri []
+  (-> (.. @vscode -workspace -workspaceFolders)
+      first ;; Handle nil here?
+      (.. -uri)))
+
+(defn get-project-root-uri
+  ([]
+   (get-project-root-uri true))
+  ([use-cache]
+   (if use-cache
+     (if-let [project-directory-uri (state/get-state-value project-root-uri-key)]
+       project-directory-uri
+       (get-first-workspace-folder-uri))
+     (get-first-workspace-folder-uri))))
 
 (defn activate [^js vsc]
   (reset! vscode vsc)
@@ -10,6 +28,9 @@
 (defn create-repl-output-file [])
 
 (comment
+  (get-project-root-uri)
+  @state/state
+  (or true false false)
   (run!
    #(.. fs (appendFileSync
             "/Users/brandon/development/crescent-api/.calva/output-window/repl-output.md"
