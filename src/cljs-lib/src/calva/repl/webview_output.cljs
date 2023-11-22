56087e9d1746cef8ace510afa6014e84d8daa878
Brandon Ringe <12722744+bpringe@users.noreply.github.com>
2023-11-22 14:23:21 -0800

    Begin setup for using reagent in output webview - WIP

--- src/cljs-lib/src/calva/repl/webview_output.cljs
+++ src/cljs-lib/src/calva/repl/webview_output.cljs
@@ -21,6 +21,63 @@
 (defn get-webview-html-path []
   (.. path (join (.. @util/context -extensionPath) "assets" "repl-output-webview.html")))
 
+(defn get-webview-html
+  [js-src]
+  (str "
+<!DOCTYPE html>
+<html lang=\"en\">
+  <head>
+    <meta charset=\"UTF-8\" />
+    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\" />
+    <title>REPL Output</title>
+    <link
+      rel=\"stylesheet\"
+      href=\"https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/atom-one-dark.min.css\"
+    />
+    <script src=\"https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/highlight.min.js\"></script>
+    <script src=\"https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/languages/clojure.min.js\"></script>
+    <script>
+      hljs.highlightAll();
+    </script>
+  </head>
+  <body>
+    <img src=\"https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif\" width=\"300\" />
+
+    <pre><code class=\"language-clojure\">
+(defn hello-world [] (println \"highlightjs code block\"))
+    </code></pre>
+
+    <pre>
+      <code class=\"language-clojure\">
+(defn hello-world [] (println \"highlightjs code block\"))
+      </code>
+    </pre>
+
+    <!-- TODO: Disable inline scripts - see security section in webview docs -->
+    <div id=\"results\"></div>
+
+    <script>
+      window.addEventListener('message', (event) => {
+        const message = event.data; // The JSON data our extension sent
+        const resultsDiv = document.getElementById('results');
+
+        switch (message.command) {
+          case 'print-result':
+            const resultParagraph = document.createElement('p');
+            const text = document.createTextNode(message.result);
+            resultParagraph.appendChild(text);
+            resultsDiv.appendChild(resultParagraph);
+            break;
+          case 'clear-results':
+            resultsDiv.innerHTML = '';
+            break;
+        }
+      });
+    </script>
+    <script src=\"" js-src "\"></script>
+  </body>
+</html>"))
+
 (defn get-webview-content []
   (.. fs (readFileSync (get-webview-html-path)
                        "utf-8")))
@@ -30,6 +87,8 @@
       -webview
       (postMessage (clj->js message))))
 
+(defn main [])
+
 ;; TODO: See if can send repl output to webview when it's hidden and see it once unhidden
 ;; "You cannot send messages to a hidden webview, even when retainContextWhenHidden is enabled."
 ;; https://code.visualstudio.com/api/extension-guides/webview#theming-webview-content
@@ -37,12 +96,11 @@
 (comment
   (create-repl-output-webview-panel)
 
-  (set! (.. @repl-output-webview-panel -webview -html) (get-webview-content))
+  @(def js-path (.. @util/vscode -Uri (joinPath (.. @util/context -extensionUri) "assets" "js" "repl-output-webview.js")))
 
-  (.. @repl-output-webview-panel
-      -webview
-      (postMessage #js {:command "print-result"
-                        :result "Hello world!!!"}))
+  @(def js-src (.. @repl-output-webview-panel -webview (asWebviewUri js-path)))
+
+  (set! (.. @repl-output-webview-panel -webview -html) (get-webview-html js-src))
 
   (post-message-to-webview {:command "print-result"
                             :result "Hello world!!!"})
