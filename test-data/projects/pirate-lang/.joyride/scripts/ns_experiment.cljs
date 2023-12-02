679b07bc232fda28d82980222d8fd5e5a7f009d2
Peter Strömberg <pez@pezius.com>
2023-12-02 13:42:50 +0100

    Update ns-experiments joyride code

--- test-data/projects/pirate-lang/.joyride/scripts/ns_experiment.cljs
+++ test-data/projects/pirate-lang/.joyride/scripts/ns_experiment.cljs
@@ -1,6 +1,10 @@
 (ns ns-experiment
   (:require ["ext://betterthantomorrow.calva$v0" :as calva]))
 
-(calva/repl.evaluateCode "clj" (str '(do (println "Hello printed") "Hello a joyride eval")))
+(-> (calva/repl.evaluateCode "clj" (str '(do (println "Hello printed") "Hello a joyride eval")))
+    (.then (fn [result]
+             (println (.-output result)))))
 
-#_(calva/repl.evaluateCode "clj" (pr-str (list 'println "Hello printed")) {} #js {:ns "user"})
+(calva/repl.evaluateCode "clj" (pr-str (list 'println "Hello printed")) {} #js {:ns "user"})
+
+(calva/repl.evaluateCode js/undefined (str '(do (println "Hello printed") "Hello a joyride eval")))
