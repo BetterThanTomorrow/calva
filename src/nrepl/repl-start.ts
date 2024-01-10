ac91c1f22a3ba6c08a7ddcaf06b486514d13334b
Peter Strömberg <pez@pezius.com>
2024-01-10 09:57:32 +0100

    Separate menu composition functions for connected vs disconnected

--- src/nrepl/repl-start.ts
+++ src/nrepl/repl-start.ts
@@ -188,13 +188,58 @@ export async function startStandaloneRepl(
   });
 }
 
-function composeMenu() {
-  const JACK_IN_OPTION = 'Start your project with a REPL and connect (a.k.a. Jack-in)';
-  const JACK_IN_COMMAND = 'calva.jackIn';
+function composeConnectedMenu() {
   const RE_JACK_IN_OPTION = 'Restart the Project REPL (a.k.a. Re-jack-in)';
   const RE_JACK_IN_COMMAND = 'calva.jackIn';
   const JACK_OUT_OPTION = 'Stop/Kill the Project REPL started by Calva (a.k.a. Jack-out)';
   const JACK_OUT_COMMAND = 'calva.jackOut';
+  const INTERRUPT_OPTION = 'Interrupt running Evaluations';
+  const INTERRUPT_COMMAND = 'calva.interruptAllEvaluations';
+  const DISCONNECT_OPTION = 'Disconnect from the REPL';
+  const DISCONNECT_COMMAND = 'calva.disconnect';
+  const OPEN_WINDOW_OPTION = 'Open the Output Window';
+  const OPEN_WINDOW_COMMAND = 'calva.showOutputWindow';
+  const OPEN_FIDDLE_OPTION = 'Open Fiddle for Current File';
+  const OPEN_FIDDLE_COMMAND = 'calva.openFiddleForSourceFile';
+  const EVALUATE_FIDDLE_OPTION = 'Evaluate Fiddle for Current File';
+  const EVALUATE_FIDDLE_COMMAND = 'calva.evaluateFiddleForSourceFile';
+  const OPEN_SOURCE_FOR_FIDDLE_OPTION = 'Open Source File for Current Fiddle';
+  const OPEN_SOURCE_FOR_FIDDLE_COMMAND = 'calva.openSourceFileForFiddle';
+  const PREFERRED_ORDER = [
+    INTERRUPT_OPTION,
+    OPEN_WINDOW_OPTION,
+    RE_JACK_IN_OPTION,
+    DISCONNECT_OPTION,
+    JACK_OUT_OPTION,
+    OPEN_FIDDLE_OPTION,
+    EVALUATE_FIDDLE_OPTION,
+    OPEN_SOURCE_FOR_FIDDLE_OPTION,
+  ];
+
+  const commands = {};
+  if (fiddleFiles.activeEditorIsFiddle) {
+    commands[OPEN_SOURCE_FOR_FIDDLE_OPTION] = OPEN_SOURCE_FOR_FIDDLE_COMMAND;
+  } else {
+    commands[OPEN_FIDDLE_OPTION] = OPEN_FIDDLE_COMMAND;
+  }
+  commands[INTERRUPT_OPTION] = INTERRUPT_COMMAND;
+  commands[DISCONNECT_OPTION] = DISCONNECT_COMMAND;
+  if (replSession.getSession('clj')) {
+    commands[OPEN_WINDOW_OPTION] = OPEN_WINDOW_COMMAND;
+  }
+  if (utilities.getJackedInState()) {
+    commands[RE_JACK_IN_OPTION] = RE_JACK_IN_COMMAND;
+    commands[JACK_OUT_OPTION] = JACK_OUT_COMMAND;
+  }
+  if (!fiddleFiles.activeEditorIsFiddle) {
+    commands[EVALUATE_FIDDLE_OPTION] = EVALUATE_FIDDLE_COMMAND;
+  }
+  return { commands, PREFERRED_ORDER };
+}
+
+function composeDisconnectedMenu() {
+  const JACK_IN_OPTION = 'Start your project with a REPL and connect (a.k.a. Jack-in)';
+  const JACK_IN_COMMAND = 'calva.jackIn';
   const START_REPL_OPTION = 'Start a standalone REPL';
   const START_REPL_COMMAND = 'calva.startStandaloneRepl';
   const START_JOYRIDE_REPL_OPTION = 'Start a Joyride REPL and Connect';
@@ -209,18 +254,6 @@ function composeMenu() {
   const CONNECT_PROJECT_COMMAND = 'calva.connect';
   const CONNECT_STANDALONE_OPTION = 'Connect to a running REPL, not in your project';
   const CONNECT_STANDALONE_COMMAND = 'calva.connectNonProjectREPL';
-  const INTERRUPT_OPTION = 'Interrupt running Evaluations';
-  const INTERRUPT_COMMAND = 'calva.interruptAllEvaluations';
-  const DISCONNECT_OPTION = 'Disconnect from the REPL';
-  const DISCONNECT_COMMAND = 'calva.disconnect';
-  const OPEN_WINDOW_OPTION = 'Open the Output Window';
-  const OPEN_WINDOW_COMMAND = 'calva.showOutputWindow';
-  const OPEN_FIDDLE_OPTION = 'Open Fiddle for Current File';
-  const OPEN_FIDDLE_COMMAND = 'calva.openFiddleForSourceFile';
-  const EVALUATE_FIDDLE_OPTION = 'Evaluate Fiddle for Current File';
-  const EVALUATE_FIDDLE_COMMAND = 'calva.evaluateFiddleForSourceFile';
-  const OPEN_SOURCE_FOR_FIDDLE_OPTION = 'Open Source File for Current Fiddle';
-  const OPEN_SOURCE_FOR_FIDDLE_COMMAND = 'calva.openSourceFileForFiddle';
   const PREFERRED_ORDER = [
     JACK_IN_OPTION,
     CONNECT_PROJECT_OPTION,
@@ -230,59 +263,34 @@ function composeMenu() {
     START_HELLO_REPL_OPTION,
     START_HELLO_CLJS_BROWSER_OPTION,
     START_HELLO_CLJS_NODE_OPTION,
-    INTERRUPT_OPTION,
-    OPEN_WINDOW_OPTION,
-    RE_JACK_IN_OPTION,
-    DISCONNECT_OPTION,
-    JACK_OUT_OPTION,
-    OPEN_FIDDLE_OPTION,
-    EVALUATE_FIDDLE_OPTION,
-    OPEN_SOURCE_FOR_FIDDLE_OPTION,
   ];
+
   const commands = {};
-  if (fiddleFiles.activeEditorIsFiddle) {
-    commands[OPEN_SOURCE_FOR_FIDDLE_OPTION] = OPEN_SOURCE_FOR_FIDDLE_COMMAND;
-  } else {
-    commands[OPEN_FIDDLE_OPTION] = OPEN_FIDDLE_COMMAND;
-  }
-  if (
-    !utilities.getConnectedState() &&
-    !utilities.getConnectingState() &&
-    !utilities.getLaunchingState()
-  ) {
-    if (vscode.workspace.workspaceFolders && vscode.workspace.workspaceFolders.length > 0) {
-      if (vscode.workspace.workspaceFolders[0].uri.scheme != 'vsls') {
-        commands[JACK_IN_OPTION] = JACK_IN_COMMAND;
-        commands[CONNECT_STANDALONE_OPTION] = CONNECT_STANDALONE_COMMAND;
-      }
-      commands[CONNECT_PROJECT_OPTION] = CONNECT_PROJECT_COMMAND;
-    } else {
+  if (vscode.workspace.workspaceFolders && vscode.workspace.workspaceFolders.length > 0) {
+    if (vscode.workspace.workspaceFolders[0].uri.scheme != 'vsls') {
+      commands[JACK_IN_OPTION] = JACK_IN_COMMAND;
       commands[CONNECT_STANDALONE_OPTION] = CONNECT_STANDALONE_COMMAND;
-      commands[START_REPL_OPTION] = START_REPL_COMMAND;
     }
-    commands[START_JOYRIDE_REPL_OPTION] = START_JOYRIDE_REPL_COMMAND;
-    commands[START_HELLO_REPL_OPTION] = START_HELLO_REPL_COMMAND;
-    commands[START_HELLO_CLJS_BROWSER_OPTION] = START_HELLO_CLJS_BROWSER_COMMAND;
-    commands[START_HELLO_CLJS_NODE_OPTION] = START_HELLO_CLJS_NODE_COMMAND;
+    commands[CONNECT_PROJECT_OPTION] = CONNECT_PROJECT_COMMAND;
   } else {
-    commands[INTERRUPT_OPTION] = INTERRUPT_COMMAND;
-    commands[DISCONNECT_OPTION] = DISCONNECT_COMMAND;
-    if (replSession.getSession('clj')) {
-      commands[OPEN_WINDOW_OPTION] = OPEN_WINDOW_COMMAND;
-    }
-    if (utilities.getJackedInState()) {
-      commands[RE_JACK_IN_OPTION] = RE_JACK_IN_COMMAND;
-      commands[JACK_OUT_OPTION] = JACK_OUT_COMMAND;
-    }
-    if (!fiddleFiles.activeEditorIsFiddle) {
-      commands[EVALUATE_FIDDLE_OPTION] = EVALUATE_FIDDLE_COMMAND;
-    }
+    commands[CONNECT_STANDALONE_OPTION] = CONNECT_STANDALONE_COMMAND;
+    commands[START_REPL_OPTION] = START_REPL_COMMAND;
   }
+  commands[START_JOYRIDE_REPL_OPTION] = START_JOYRIDE_REPL_COMMAND;
+  commands[START_HELLO_REPL_OPTION] = START_HELLO_REPL_COMMAND;
+  commands[START_HELLO_CLJS_BROWSER_OPTION] = START_HELLO_CLJS_BROWSER_COMMAND;
+  commands[START_HELLO_CLJS_NODE_OPTION] = START_HELLO_CLJS_NODE_COMMAND;
   return { commands, PREFERRED_ORDER };
 }
 
 export function startOrConnectRepl() {
-  const { commands, PREFERRED_ORDER } = composeMenu();
+  const showConnectedMenu =
+    utilities.getConnectedState() ||
+    utilities.getConnectingState() ||
+    utilities.getLaunchingState();
+  const { commands, PREFERRED_ORDER } = showConnectedMenu
+    ? composeConnectedMenu()
+    : composeDisconnectedMenu();
 
   const sortedCommands = utilities.sortByPresetOrder(Object.keys(commands), PREFERRED_ORDER);
   void vscode.window.showQuickPick(sortedCommands).then((v) => {
