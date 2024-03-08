f0ff842868f9f7f3bed2319e2eb264d618f2e3c4
Peter Strömberg <pez@pezius.com>
2024-03-08 09:35:43 +0100

    Clean away some more unused stuff

--- src/analytics.ts
+++ src/analytics.ts
@@ -1,7 +1,6 @@
 import * as vscode from 'vscode';
 import axios from 'axios';
 import * as uuid from 'uuidv4';
-import * as os from 'os';
 import { isUndefined } from 'lodash';
 
 function userAllowsTelemetry(): boolean {
@@ -10,21 +9,11 @@ function userAllowsTelemetry(): boolean {
 }
 
 export default class Analytics {
-  private extension: vscode.Extension<any>;
-  private extensionVersion: string;
   private store: vscode.Memento;
-  private GA_ID = (process.env.CALVA_DEV_GA
-    ? process.env.CALVA_DEV_GA
-    : 'FUBAR-69796730-3'
-  ).replace(/^FUBAR/, 'UA');
   private GA4_TOKEN = process.env.CALVA_DEV_GA4_TOKEN ?? 'GgrUWszmTo2FG538YCUGpw';
   private GA4_MEASUREMENT_ID = process.env.CALVA_DEV_GA4_ID ?? 'G-HYZ3MX6DL1';
-  private ua: string;
 
   constructor(context: vscode.ExtensionContext) {
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    this.extension = vscode.extensions.getExtension('betterthantomorrow.calva')!;
-    this.extensionVersion = this.extension.packageJSON.version;
     this.store = context.globalState;
   }
 
@@ -44,10 +33,6 @@ export default class Analytics {
     if (!userAllowsTelemetry()) {
       return;
     }
-    const userAgent = `Mozilla/5.0 (${os.platform()}; ${os.release()}; ${os.type}) Code/${
-      vscode.version
-    } Calva/${this.extensionVersion}`;
-
     try {
       return axios
         .post(
@@ -68,7 +53,7 @@ export default class Analytics {
           },
           {
             headers: {
-              'User-Agent': userAgent,
+              'User-Agent': 'Mozilla/5.0 VSCode',
               'Content-Type': 'application/json',
             },
           }
