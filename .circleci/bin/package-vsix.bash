040f3aada012f915e4471c5543907668d933f280
Ingy döt Net <ingy@ingy.net>
2024-04-13 14:53:32 -0700

    Fix a Bash bug I introduced getting shellcheck to pass

--- .circleci/bin/package-vsix.bash
+++ .circleci/bin/package-vsix.bash
@@ -1,10 +1,10 @@
 if [[ $CIRCLE_TAG =~ ^v([0-9]+\.[0-9]+\.[0-9]+)$ ]]; then
   echo 'Packaging as release'
-  PACKAGE_CMD='vsce package --githubBranch published'
+  PACKAGE_CMD=(vsce package --githubBranch published)
 
 else
   echo 'Packaging as pre-release'
-  PACKAGE_CMD='vsce package --pre-release'
+  PACKAGE_CMD=(vsce package --pre-release)
 fi
 
-npx "$PACKAGE_CMD"
+npx "${PACKAGE_CMD[@]}"
