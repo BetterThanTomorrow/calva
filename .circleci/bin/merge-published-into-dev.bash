43ded9f4f4e1b64ee86fad66f4aa9ab4f1ce5d9c
Ingy döt Net <ingy@ingy.net>
2024-04-17 22:11:51 -0700

    Refactor bash and get tests passing after last refactor

--- .circleci/bin/merge-published-into-dev.bash
+++ .circleci/bin/merge-published-into-dev.bash
@@ -1,5 +1,5 @@
-git config --global user.email $GITHUB_USER_EMAIL
-git config --global user.name $GITHUB_USER_NAME
+git config --global user.email "$GITHUB_USER_EMAIL"
+git config --global user.name "$GITHUB_USER_NAME"
 git checkout dev
 git pull
 git merge origin/published --no-ff -m "Merge branch published into dev [skip ci]"
