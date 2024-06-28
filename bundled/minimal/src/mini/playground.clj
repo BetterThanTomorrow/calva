7943309b3e5a01b0e34ad367125107f13242a3d6
Peter Strömberg <pez@pezius.com>
2024-06-28 09:34:13 +0200

    Update mini-settings documentation

--- bundled/minimal/src/mini/playground.clj
+++ bundled/minimal/src/mini/playground.clj
@@ -1,6 +1,7 @@
 (ns mini.playground)
 
-; This project has custom configuration. See .vscode/settings.json
+; This project has custom configuration.
+; See .vscode/settings.json
 
 ; If you are new to Calva, you may want to use the command:
 ; Calva: Create a “Getting Started” REPL project
