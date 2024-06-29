81e337a2ac4684cbd9f154caebc528ceb663ea2e
Peter Strömberg <pez@pezius.com>
2024-06-29 16:45:06 +0200

    Remove connect sequence from dram config

--- src/nrepl/dram-repl.ts
+++ src/nrepl/dram-repl.ts
@@ -20,7 +20,6 @@ export type DramConfig = {
 
 type DramTemplate = {
   config: DramConfig | string;
-  connectSequence: ReplConnectSequence;
 };
 
 export const USER_TEMPLATE: DramTemplate = {
@@ -33,22 +32,18 @@ export const USER_TEMPLATE: DramTemplate = {
       { path: '.vscode/settings.json', 'open?': false },
     ],
   },
-  connectSequence: null, //sequence.cljDefaults[0],
 };
 
 export const HELLO_TEMPLATE: DramTemplate = {
   config: 'calva_getting_started',
-  connectSequence: sequence.cljDefaults[0],
 };
 
 export const HELLO_CLJS_BROWSER_TEMPLATE: DramTemplate = {
   config: 'calva_cljs_browser_quick_start',
-  connectSequence: sequence.cljDefaults[3],
 };
 
 export const HELLO_CLJS_NODE_TEMPLATE: DramTemplate = {
   config: 'calva_cljs_node_quick_start',
-  connectSequence: sequence.cljDefaults[4],
 };
 
 const calculateBranch = () => {
@@ -307,7 +302,7 @@ export async function startDramRepl() {
   const args = await deserializeDramReplStartConfig(state.getProjectRootUri());
   console.debug('Dram repl start config:', args);
   void vscode.workspace.fs.delete(ARGS_FILE_PATH(state.getProjectRootUri()));
-  await state.initProjectDir(ConnectType.JackIn, args.dramTemplate.connectSequence, false);
+  await state.initProjectDir(ConnectType.JackIn, null, false);
   const projectRootUri = state.getProjectRootUri();
   const [mainDoc, mainEditor] = await openStoredDoc(projectRootUri, args.config.files[0]);
   for (const file of args.config.files.slice(1)) {
