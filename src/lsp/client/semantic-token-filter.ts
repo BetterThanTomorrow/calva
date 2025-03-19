7fdc87af048dd76a58e88d12f0ffeb648d563aaa
PEZ <pez@pezius.com>
2025-03-19 09:16:18 +0100

    Clarify comment type

--- src/lsp/client/semantic-token-filter.ts
+++ src/lsp/client/semantic-token-filter.ts
@@ -1,15 +1,20 @@
+// Token type 10 (:comment) as defined in clojure-lsp's token-types vector
+const COMMENT_TOKEN_TYPE = 10;
+
 export function filterCommentTokens(data: Uint32Array) {
   const filteredData: number[] = [];
   let accumulatedDeltaLine = 0;
 
   for (let i = 0; i < data.length; i += 5) {
-    const deltaLine = data[i];
-    const deltaStart = data[i + 1];
-    const length = data[i + 2];
-    const tokenType = data[i + 3];
-    const modifiers = data[i + 4];
+    const [deltaLine, deltaStart, length, tokenType, modifiers] = [
+      data[i],
+      data[i + 1],
+      data[i + 2],
+      data[i + 3],
+      data[i + 4],
+    ];
 
-    if (tokenType === 10) {
+    if (tokenType === COMMENT_TOKEN_TYPE) {
       accumulatedDeltaLine += deltaLine;
       continue;
     }
