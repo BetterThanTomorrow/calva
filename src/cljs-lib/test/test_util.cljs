ed53d2d88d5268cbf86266ef4dfc2213374f0a8f
Brandon Ringe <12722744+bpringe@users.noreply.github.com>
2025-05-04 18:22:23 -0700

    Update docstring

--- src/cljs-lib/test/test_util.cljs
+++ src/cljs-lib/test/test_util.cljs
@@ -2,8 +2,9 @@
 
 (defn wrap-spy
   "This is a helper that returns a function that calls the spy (created by the tortue/spy library),
-   so that the shadow-cljs doesn't complain, which is does if a spy is used and called directly in a test - it will say
+   so that the shadow-cljs doesn't complain, which it does if a spy is used and called directly in a test - it will say
    the thing is not a function.
+
    See:
    - https://github.com/alexanderjamesking/spy/issues/29
    - https://clojurescript.org/reference/compiler-options#static-fns
