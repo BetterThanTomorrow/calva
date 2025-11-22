bc65eeffa47c4305e3d7d509a5dcb39271851eb0
PEZ <pez@pezius.com>
2025-11-22 17:45:46 +0100

    Fix failing highlight test

--- src/extension-test/integration/suite/highlight-test.ts
+++ src/extension-test/integration/suite/highlight-test.ts
@@ -6,6 +6,19 @@ import * as testUtil from './util';
 import * as vscode from 'vscode';
 import * as highlight from '../../../highlight/src/extension';
 
+async function waitForCondition(predicate: () => boolean, timeoutMs = 4000, intervalMs = 50) {
+  const start = Date.now();
+  while (true) {
+    if (predicate()) {
+      return;
+    }
+    if (Date.now() - start > timeoutMs) {
+      throw new Error('Timed out waiting for condition');
+    }
+    await testUtil.sleep(intervalMs);
+  }
+}
+
 suite('Highlight suite', () => {
   const suite = 'Highlight';
 
@@ -20,15 +33,18 @@ suite('Highlight suite', () => {
   test('activeEditor', async function () {
     testUtil.log(suite, 'activeEditor');
 
-    assert.strictEqual(highlight.activeEditor, undefined);
-
+    const initialEditor = highlight.activeEditor;
     const testFilePath = path.join(testUtil.testDataDir, 'test.clj');
+
     await testUtil.openFile(testFilePath);
     testUtil.log(suite, 'test.clj opened');
 
-    assert.notStrictEqual(highlight.activeEditor, undefined);
+    await waitForCondition(() => highlight.activeEditor?.document.uri.fsPath === testFilePath);
+    assert.strictEqual(highlight.activeEditor?.document.uri.fsPath, testFilePath);
 
     await vscode.commands.executeCommand('workbench.action.closeActiveEditor');
     testUtil.log(suite, 'test.clj closed');
+
+    await waitForCondition(() => highlight.activeEditor?.document.uri.fsPath !== testFilePath);
   });
 });
