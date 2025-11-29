294c5c0d986a77309f9d6bd22bfc200b7154d4a9
PEZ <pez@pezius.com>
2025-11-29 02:28:52 +0100

    Make info API namespace use router

--- src/api/info.ts
+++ src/api/info.ts
@@ -1,14 +1,11 @@
 import { clojureDocsCiderNReplLookup } from '../clojuredocs';
+import * as replSession from '../nrepl/repl-session';
 import * as sessionRegistry from '../nrepl/session-registry';
-import * as clientRegistry from '../nrepl/client-registry';
 
 // TODO: Only nRepl lookups for now. Figure out how to enable clojure-lsp.
 
 export const getClojureDocsDotOrg = async (symbol: string, ns = 'user') => {
-  const activeClientKey = clientRegistry.getActiveClientKey();
-  const session = activeClientKey
-    ? sessionRegistry.getPrimarySessionForClient(activeClientKey)
-    : null;
+  const session = replSession.getSession();
   if (!session) {
     return { error: "Can't retrieve REPL session for session key. Is the REPL connected?" };
   }
