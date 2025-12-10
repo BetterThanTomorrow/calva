6e740de7ad6a73a31ad4cfdeeebd20b634279bbf
PEZ <pez@pezius.com>
2025-12-10 17:04:52 +0100

    Make tic-tac-toe flare work in the test-data project

--- test-data/projects/scittle-replicant-tic-tac-toe/.joyride/src/ttt_flare.cljs
+++ test-data/projects/scittle-replicant-tic-tac-toe/.joyride/src/ttt_flare.cljs
@@ -2,28 +2,31 @@
   (:require
    [joyride.flare :as flare]))
 
+(defn project-path [path]
+  (str "projects/scittle-replicant-tic-tac-toe/" path))
+
 (defn replicant-ttt []
   (flare/flare!+
    {:html [:html
            [:head
             [:script "var SCITTLE_NREPL_WEBSOCKET_PORT = 1340;
                       var SCITTLE_NREPL_WEBSOCKET_HOST = '127.0.0.1';"]
-            [:script {:src "resources/scittle/js/scittle.js"
+            [:script {:src (project-path "resources/scittle/js/scittle.js")
                       :type "application/javascript"}]
             #_[:script {:src "https://cdn.jsdelivr.net/npm/scittle@0.7.28/dist/scittle.nrepl.js"
                         :type "application/javascript"}]
-            [:script {:src "resources/scittle/js/scittle.nrepl.js"
+            [:script {:src (project-path "resources/scittle/js/scittle.nrepl.js")
                       :type "application/javascript"}]
-            [:script {:src "resources/scittle/js/scittle.replicant.js"
+            [:script {:src (project-path "resources/scittle/js/scittle.replicant.js")
                       :type "application/javascript"}]
             [:script {:type "application/x-scittle"
-                      :src "resources/scittle/replicant_tictactoe/ui.cljs"}]
+                      :src (project-path "resources/scittle/replicant_tictactoe/ui.cljs")}]
             [:script {:type "application/x-scittle"
-                      :src "resources/scittle/replicant_tictactoe/game.cljs"}]
+                      :src (project-path "resources/scittle/replicant_tictactoe/game.cljs")}]
             [:script {:type "application/x-scittle"
-                      :src "resources/scittle/replicant_tictactoe/core.cljs"}]
+                      :src (project-path "resources/scittle/replicant_tictactoe/core.cljs")}]
             [:link {:rel "stylesheet"
-                    :href "resources/scittle/replicant_tictactoe/style.css"}]]
+                    :href (project-path "resources/scittle/replicant_tictactoe/style.css")}]]
            [:body
             [:h1 "Scittle tic-tac-toe built with Replicant"]
             [:ul {:style {:list-style :none
