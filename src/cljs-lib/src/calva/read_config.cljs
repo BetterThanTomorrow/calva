9dcacae5d3adb67098190d590f7947e66da8f2ec
jramosg <jonurnieta@gmail.com>
2026-02-02 08:21:16 +0100

    Add kebab-case to camelCase transformation functions
    This update introduces functions to convert kebab-case strings and
    keywords to camelCase. It also includes a recursive transformation
    for map keys, enhancing the configuration parsing process to
    support camelCase keys.

--- src/cljs-lib/src/calva/read_config.cljs
+++ src/cljs-lib/src/calva/read_config.cljs
@@ -1,15 +1,55 @@
 (ns calva.read-config
   (:require [calva.js-utils :refer [jsify]]
             [calva.parse :as parse]
+            [clojure.string :as str]
+            [clojure.walk :as walk]
             [vvvvalvalval.supdate.api :refer [supdate]]))
 
-(def ^:private updaters {:customREPLCommandSnippets [{:snippet str}]
-                         :customREPLHoverSnippets   [{:snippet str}]})
+(def ^:private updaters {:customReplCommandSnippets [{:snippet str}]
+                         :customReplHoverSnippets   [{:snippet str}]
+                         :customREPLCommandSnippets [{:snippet str}]  ;; backward compat
+                         :customREPLHoverSnippets   [{:snippet str}]  ;; backward compat
+                         })
+
+(defn- kebab->camel
+  "Converts a kebab-case string to camelCase.
+   'custom-pair-forms' => 'customPairForms'"
+  [s]
+  (let [[first-part :as parts] (str/split s #"-")]
+    (->> parts rest (map str/capitalize) (cons first-part) (apply str))))
+
+(defn- kebab-keyword->camel-keyword
+  "Converts a kebab-case keyword to camelCase keyword.
+   :custom-pair-forms => :customPairForms
+   Preserves namespaced keywords (e.g., :foo/bar-baz => :foo/barBaz)"
+  [kw]
+  (let [ns-part (namespace kw)
+        name-part (name kw)
+        camel-name (kebab->camel name-part)]
+    (if ns-part
+      (keyword ns-part camel-name)
+      (keyword camel-name))))
+
+(defn- transform-keys-to-camel
+  "Recursively transforms all map keys from kebab-case to camelCase"
+  [m]
+  (walk/postwalk
+    (fn [x]
+      (if (map? x)
+        (into {} (map (fn [[k v]]
+                        [(if (keyword? k)
+                           (kebab-keyword->camel-keyword k)
+                           k)
+                         v])
+                      x))
+        x))
+    m))
 
 (defn update-config-edn
   [config]
   (-> config
       parse/parse-clj-edn
+      transform-keys-to-camel
       (supdate updaters)))
 
 (defn config-edn->js
