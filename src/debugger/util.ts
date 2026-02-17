4ed8f6399df6294104effb67bfb2126237e859d2
jramosg <jonurnieta@gmail.com>
2026-02-17 09:42:05 +0100

    Fix token cursor initialization in breakpoint function
    Ensure the token cursor is set to the correct starting position
    using the defunStart value. This change improves the accuracy
    of the cursor movement when locating breakpoints in the code.

--- src/debugger/util.ts
+++ src/debugger/util.ts
@@ -22,7 +22,8 @@ function moveTokenCursorToBreakpoint(
   debugResponse: any
 ): LispTokenCursor {
   const errorMessage = 'Error finding position of breakpoint';
-  const [_, defunEnd] = tokenCursor.rangeForDefun(tokenCursor.offsetStart);
+  const [defunStart, defunEnd] = tokenCursor.rangeForDefun(tokenCursor.offsetStart);
+  tokenCursor.set(tokenCursor.doc.getTokenCursor(defunStart));
   let inSyntaxQuote = false;
 
   const coor = [...debugResponse.coor]; // Copy the array so we do not modify the one stored in state
