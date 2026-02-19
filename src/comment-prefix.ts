db30f673e2f71bfe10f3682f6737bbffc386391a
jramosg <jonurnieta@gmail.com>
2026-02-19 09:55:45 +0100

    Comment prefix removal logic
    Adjusted the handling of trailing spaces after
    semicolons to ensure accurate removal of comment prefixes.
    Fixes Toggle comments off, indent glitch
    Fixes #3078

--- src/comment-prefix.ts
+++ src/comment-prefix.ts
@@ -1,6 +1,15 @@
 /** Matches one or more leading semicolons (`;`, `;;`, `;;;`, etc.) */
 export const commentPrefixPattern = /^;+/;
 
+/**
+ * Calculates the end index for removing a comment prefix (semicolons + trailing space)
+ * from a line of text. Returns the index up to which characters should be removed,
+ * accounting for the semicolon(s) and a single trailing space
+ *
+ * @param lineText - The full text of the line
+ * @param firstNonWhitespace - The index of the first non-whitespace character in the line
+ * @returns The end index for removal, or `undefined` if no comment prefix is found
+ */
 export function calculateCommentPrefixRemovalEnd(
   lineText: string,
   firstNonWhitespace: number
@@ -12,7 +21,8 @@ export function calculateCommentPrefixRemovalEnd(
   }
 
   let removalEnd = firstNonWhitespace + match[0].length;
-  while (removalEnd < lineText.length && lineText[removalEnd] === ' ') {
+  // If there is a space immediately following the semicolons, include it in the removal
+  if (removalEnd < lineText.length && lineText[removalEnd] === ' ') {
     removalEnd++;
   }
 
