46947b52c9c190c9515cab135c0e8359c52f4ee2
PEZ <pez@pezius.com>
2026-05-02 08:38:50 +0200

    Update scittle CDN version to 0.8.31 in test project

--- test-data/projects/scittle-replicant-tic-tac-toe/.joyride/src/ttt_flare.cljs
+++ test-data/projects/scittle-replicant-tic-tac-toe/.joyride/src/ttt_flare.cljs
@@ -11,11 +11,11 @@
            [:head
             [:script "var SCITTLE_NREPL_WEBSOCKET_PORT = 1340;
                       var SCITTLE_NREPL_WEBSOCKET_HOST = '127.0.0.1';"]
-            [:script {:src "https://cdn.jsdelivr.net/npm/scittle@0.7.30/dist/scittle.js"
+            [:script {:src "https://cdn.jsdelivr.net/npm/scittle@0.8.31/dist/scittle.js"
                       :type "application/javascript"}]
-            [:script {:src "https://cdn.jsdelivr.net/npm/scittle@0.7.30/dist/scittle.nrepl.js"
+            [:script {:src "https://cdn.jsdelivr.net/npm/scittle@0.8.31/dist/scittle.nrepl.js"
                       :type "application/javascript"}]
-            [:script {:src "https://cdn.jsdelivr.net/npm/scittle@0.7.30/dist/scittle.replicant.js"
+            [:script {:src "https://cdn.jsdelivr.net/npm/scittle@0.8.31/dist/scittle.replicant.js"
                       :type "application/javascript"}]
             [:script {:type "application/x-scittle"
                       :src (project-path "resources/scittle/replicant_tictactoe/ui.cljs")}]
